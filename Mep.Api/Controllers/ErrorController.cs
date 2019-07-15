9e84ec748fdfdb17093084ca131f8738084f3893
Neil B Davies <neil.davies11@nhs.net>
2019-07-15 16:03:13 +0100

    adding simple error controller

---
+++ Mep.Api/Controllers/ErrorController.cs
@@ -0,0 +1,46 @@
+using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Mep.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ErrorController : ControllerBase
+    {
+        private ILogger _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("")]
+        [AllowAnonymous]
+        public IActionResult Get()
+        {
+            // Get the details of the exception that occurred
+            var exceptionFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            if (exceptionFeature != null)
+            {
+                // Get which route the exception occurred at
+                string routeWhereExceptionOccurred = exceptionFeature.Path;
+
+                // Get the exception that occurred
+                Exception exceptionThatOccurred = exceptionFeature.Error;
+                
+                _logger.LogError(exceptionThatOccurred, routeWhereExceptionOccurred);
+
+                return StatusCode(StatusCodes.Status500InternalServerError, exceptionThatOccurred.Message);
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }            
+        }
+    }
+}
--- Mep.Api/Startup.cs
+++ Mep.Api/Startup.cs
@@ -11,52 +11,53 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Mep.Api
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-            //TODO: Add this to a config file
-            string connection = 
-                "Server=localhost" +
-                ";DataBase=MedicalExaminationsPortal;" +
-                ";Trusted_Connection=true" +
-                ";MultipleActiveResultSets=true;" +
-                ";Application Name=MedicalExaminationsPortal;";
-
-            services.AddDbContext<ApplicationContext>
-                (options => options.UseSqlServer(
-                    connection));
-
-            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-            services.AddScoped<ISpecialityService, SpecialityService>();         
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseMvc();
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+      //TODO: Add this to a config file
+      string connection = 
+        "Server=localhost" +
+        ";DataBase=MedicalExaminationsPortal;" +
+        ";Trusted_Connection=true" +
+        ";MultipleActiveResultSets=true;" +
+        ";Application Name=MedicalExaminationsPortal;";
+
+      services.AddDbContext<ApplicationContext>
+        (options => options.UseSqlServer(
+          connection));
+
+      services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+      services.AddScoped<ISpecialityService, SpecialityService>();     
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+
+      app.UseExceptionHandler("/Error");
+      app.UseHttpsRedirection();
+      app.UseMvc();
+    }
+  }
 }
