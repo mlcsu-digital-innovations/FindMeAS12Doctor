79bd8b11ded6a5673b0e76b18f04909d953916a2
Stephen Harland <stephen.harland1@nhs.net>
2019-07-22 12:14:45 +0100

    Referral Profile and Test

---
+++ Mep.Business/Models/Profiles/ReferralProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using Entities = Mep.Data.Entities;
+namespace Mep.Business.Models.Profiles
+{
+  public class ReferralProfile : Profile
+  {
+    public ReferralProfile()
+    {
+      CreateMap<Entities.Referral, Models.Referral>();
+
+      CreateMap<Models.Referral, Entities.Referral>();
+    }
+  }
+}
