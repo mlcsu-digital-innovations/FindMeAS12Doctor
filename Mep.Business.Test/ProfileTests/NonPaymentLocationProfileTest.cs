2714504138f9950361f4b2e5c176e9f905b84835
Mark Carter <mark.carter4@nhs.net>
2019-07-22 10:23:53 +0100

    NonPaymentLocation profile and tests

---
+++ Mep.Business.Test/ProfileTests/NonPaymentLocationProfileTest.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mep.Business.Test.ProfileTests;
+using Entities = Mep.Data.Entities;
+using System;
+namespace Mep.Business.Test
+{
+  [TestClass]
+  public class NonPaymentLocationProfileTest : GenericProfileTest<Business.Models.NonPaymentLocation, Entities.NonPaymentLocation>
+  {
+    private String[] ignoredMappings = new string[3] {
+       "ModifiedByUser",
+       "Ccg",
+       "NonPaymentLocationType"
+      };
+   
+    [TestMethod]
+    public void NonPaymentLocationProfileBusiness2EntityIsValid()
+    {      
+      AssertBusiness2EntityMappingIsValid(ignoredMappings);
+    }
+
+    [TestMethod]
+    public void NonPaymentLocationProfileEntity2BusinessIsValid()
+    {
+      AssertEntity2BusinessMappingIsValid(ignoredMappings);
+    }
+  }
+}
