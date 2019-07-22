54292ab461df2851a53b8d25b678b07e6a9679be
Mark Carter <mark.carter4@nhs.net>
2019-07-22 12:14:53 +0100

    Organisation profile and tests

---
+++ Mep.Business.Test/ProfileTests/OrganisationProfileTest.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mep.Business.Test.ProfileTests;
+using Entities = Mep.Data.Entities;
+using System;
+namespace Mep.Business.Test
+{
+  [TestClass]
+  public class OrganisationProfileTest : GenericProfileTest<Business.Models.Organisation, Entities.Organisation>
+  {
+    private String[] ignoredMappings = new string[2] {
+       "ModifiedByUser",
+       "Users"
+      };
+
+    [TestMethod]
+    public void OrganisationProfileBusiness2EntityIsValid()
+    {
+      AssertBusiness2EntityMappingIsValid(ignoredMappings);
+    }
+
+    [TestMethod]
+    public void OrganisationProfileEntity2BusinessIsValid()
+    {
+      AssertEntity2BusinessMappingIsValid(ignoredMappings);
+    }
+  }
+}
