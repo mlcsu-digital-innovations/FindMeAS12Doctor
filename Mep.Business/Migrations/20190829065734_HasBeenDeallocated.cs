fc7d386b323951a4ec8f9d2de541e6c859dfdfd7
Mark Carter <mark.carter4@nhs.net>
2019-08-29 07:58:56 +0100

    Added HasBeenDeallocated

---
+++ Mep.Business/Migrations/20190829065734_HasBeenDeallocated.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mep.business.Migrations
+{
+    public partial class HasBeenDeallocated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasBeenDeallocated",
+                table: "UserExaminationClaimsAudit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasBeenDeallocated",
+                table: "UserExaminationClaims",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasBeenDeallocated",
+                table: "UserExaminationClaimsAudit");
+
+            migrationBuilder.DropColumn(
+                name: "HasBeenDeallocated",
+                table: "UserExaminationClaims");
+        }
+    }
+}
