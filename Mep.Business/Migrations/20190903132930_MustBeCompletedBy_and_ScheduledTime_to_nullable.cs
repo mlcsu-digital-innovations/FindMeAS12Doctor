5d68b847fc173921dd320c91035681d3b4acdaae
Stephen Harland <stephen.harland1@nhs.net>
2019-09-03 14:30:17 +0100

    migration nullable MustBeCompletedBy ScheduledTime

---
+++ Mep.Business/Migrations/20190903132930_MustBeCompletedBy_and_ScheduledTime_to_nullable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mep.business.Migrations
+{
+    public partial class MustBeCompletedBy_and_ScheduledTime_to_nullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
