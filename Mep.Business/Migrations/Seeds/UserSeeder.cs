02cfbac16b6d0f0ea67a3f7d6afc506f1fa8336b
Stephen Harland <stephen.harland1@nhs.net>
2019-09-06 14:35:00 +0100

    created user seed for doctors

--- Mep.Business/Migrations/Seeds/UserSeeder.cs
+++ Mep.Business/Migrations/Seeds/UserSeeder.cs
@@ -7,13 +7,55 @@ namespace Mep.Business.Migrations.Seeds
   internal class UserSeeder : SeederBase
   {
 
-    internal UserSeeder(ApplicationContext context) 
+    internal UserSeeder(ApplicationContext context)
       : base(context)
-    {     
+    {
     }
 
     internal void SeedData()
     {
+      User user;
+      DateTimeOffset now = DateTimeOffset.Now;
+
+      if ((user =
+      _context.Users
+                .SingleOrDefault(g => g.DisplayName == "Doctor Female")) == null)
+      {
+        user = new User();
+        _context.Add(user);
+      }
+
+      user.DisplayName = "Doctor Female";
+      user.GenderTypeId = 2;
+      user.GmcNumber = null;
+      user.HasReadTermsAndConditions = true;
+      user.IdentityServerIdentifier = Guid.NewGuid().ToString();
+      user.IsActive = true;
+      user.ModifiedByUser = GetSystemAdminUser();
+      user.OrganisationId = 1;
+      user.ProfileTypeId = 1;
+      user.Section12ApprovalStatusId = null;
+      user.Section12ExpiryDate = null;
+
+      if ((user =
+      _context.Users
+                .SingleOrDefault(g => g.DisplayName == "Doctor Male")) == null)
+      {
+        user = new User();
+        _context.Add(user);
+      }
+
+      user.DisplayName = "Doctor Male";
+      user.GenderTypeId = 1;
+      user.GmcNumber = null;
+      user.HasReadTermsAndConditions = true;
+      user.IdentityServerIdentifier = Guid.NewGuid().ToString();
+      user.IsActive = true;
+      user.ModifiedByUser = GetSystemAdminUser();
+      user.OrganisationId = 1;
+      user.ProfileTypeId = 1;
+      user.Section12ApprovalStatusId = null;
+      user.Section12ExpiryDate = null;
     }
   }
 }
