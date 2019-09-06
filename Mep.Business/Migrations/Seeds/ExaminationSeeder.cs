c247e52992ec5b9bd3beea9e90a758fc67326871
Stephen Harland <stephen.harland1@nhs.net>
2019-09-06 09:06:09 +0100

    updated examination seeder

--- Mep.Business/Migrations/Seeds/ExaminationSeeder.cs
+++ Mep.Business/Migrations/Seeds/ExaminationSeeder.cs
@@ -18,23 +18,129 @@ namespace Mep.Business.Migrations.Seeds
 
       if ((examination =
       _context.Examinations
-                .SingleOrDefault(g => g.MeetingArrangementComment == "New Examination")) == null)
+                .SingleOrDefault(g => g.Address1 == "Examination Address 1")) == null)
       {
         examination = new Examination();
         _context.Add(examination);
       }
 
-      examination.IsActive = true;
-      examination.ModifiedAt = now;
-      examination.ModifiedByUser = GetSystemAdminUser();
       examination.Address1 = "Examination Address 1";
       examination.CcgId = 1;
       examination.CreatedByUser = GetSystemAdminUser();
+      examination.IsActive = true;
+      examination.ModifiedAt = now;
+      examination.ModifiedByUser = GetSystemAdminUser();
       examination.Postcode = "ST14 5PP";
-      examination.ReferralId = 11;
+      examination.ReferralId = 1;
       examination.SpecialityId = 1;
 
+      if ((examination =
+      _context.Examinations
+                .SingleOrDefault(g => g.Address1 == "Examination Address 2")) == null)
+      {
+        examination = new Examination();
+        _context.Add(examination);
+      }
 
+      examination.Address1 = "Examination Address 2";
+      examination.CcgId = 1;
+      examination.CreatedByUser = GetSystemAdminUser();
+      examination.IsActive = true;
+      examination.ModifiedAt = now;
+      examination.ModifiedByUser = GetSystemAdminUser();
+      examination.Postcode = "ST14 5PP";
+      examination.ReferralId = 1;
+      examination.SpecialityId = 1;
+
+      if ((examination =
+      _context.Examinations
+                .SingleOrDefault(g => g.Address1 == "Examination Address 3")) == null)
+      {
+        examination = new Examination();
+        _context.Add(examination);
+      }
+
+      examination.Address1 = "Examination Address 3";
+      examination.CcgId = 1;
+      examination.CreatedByUser = GetSystemAdminUser();
+      examination.IsActive = true;
+      examination.ModifiedAt = now;
+      examination.ModifiedByUser = GetSystemAdminUser();
+      examination.Postcode = "ST14 5PP";
+      examination.ReferralId = 1;
+      examination.SpecialityId = 1;
+
+      if ((examination =
+      _context.Examinations
+                .SingleOrDefault(g => g.Address1 == "Examination Address 4")) == null)
+      {
+        examination = new Examination();
+        _context.Add(examination);
+      }
+
+      examination.Address1 = "Examination Address 4";
+      examination.CcgId = 1;
+      examination.CreatedByUser = GetSystemAdminUser();
+      examination.IsActive = true;
+      examination.ModifiedAt = now;
+      examination.ModifiedByUser = GetSystemAdminUser();
+      examination.Postcode = "ST14 5PP";
+      examination.ReferralId = 1;
+      examination.SpecialityId = 1;
+
+      if ((examination =
+      _context.Examinations
+                .SingleOrDefault(g => g.Address1 == "Examination Address 5")) == null)
+      {
+        examination = new Examination();
+        _context.Add(examination);
+      }
+
+      examination.Address1 = "Examination Address 5";
+      examination.CcgId = 1;
+      examination.CreatedByUser = GetSystemAdminUser();
+      examination.IsActive = true;
+      examination.ModifiedAt = now;
+      examination.ModifiedByUser = GetSystemAdminUser();
+      examination.Postcode = "ST14 5PP";
+      examination.ReferralId = 1;
+      examination.SpecialityId = 1;
+
+      if ((examination =
+      _context.Examinations
+                .SingleOrDefault(g => g.Address1 == "Examination Address 6")) == null)
+      {
+        examination = new Examination();
+        _context.Add(examination);
+      }
+
+      examination.Address1 = "Examination Address 6";
+      examination.CcgId = 1;
+      examination.CreatedByUser = GetSystemAdminUser();
+      examination.IsActive = true;
+      examination.ModifiedAt = now;
+      examination.ModifiedByUser = GetSystemAdminUser();
+      examination.Postcode = "ST14 5PP";
+      examination.ReferralId = 1;
+      examination.SpecialityId = 1;
+
+      if ((examination =
+      _context.Examinations
+                .SingleOrDefault(g => g.Address1 == "Examination Address 7")) == null)
+      {
+        examination = new Examination();
+        _context.Add(examination);
+      }
+
+      examination.Address1 = "Examination Address 7";
+      examination.CcgId = 1;
+      examination.CreatedByUser = GetSystemAdminUser();
+      examination.IsActive = true;
+      examination.ModifiedAt = now;
+      examination.ModifiedByUser = GetSystemAdminUser();
+      examination.Postcode = "ST14 5PP";
+      examination.ReferralId = 1;
+      examination.SpecialityId = 1;
 
     }
   }
