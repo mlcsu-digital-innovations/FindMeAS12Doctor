586012fdeba201ce66953efbebfbdb8edb7332d2
Stephen Harland <stephen.harland1@nhs.net>
2019-09-06 09:06:22 +0100

    updated referral seeded

--- Mep.Business/Migrations/Seeds/ReferralSeeder.cs
+++ Mep.Business/Migrations/Seeds/ReferralSeeder.cs
@@ -16,77 +16,145 @@ namespace Mep.Business.Migrations.Seeds
       Referral referral;
       DateTimeOffset now = DateTimeOffset.Now;
 
+      // referral with a current examination with no allocated doctors or notification responses
+
       if ((referral =
       _context.Referrals
-                .SingleOrDefault(g => g.PatientId == 2 )) == null)
+                .SingleOrDefault(g => g.PatientId == 1)) == null)
       {
         referral = new Referral();
         _context.Add(referral);
       }
 
-      referral.IsActive = true;
-      referral.ModifiedAt = now;
-      referral.ModifiedByUser = GetSystemAdminUser();
       referral.CreatedAt = now;
       referral.CreatedByUser = GetSystemAdminUser();
+      referral.IsActive = true;
+      referral.IsPlannedExamination = true;
+      referral.LeadAmhpUser = GetSystemAdminUser();
+      referral.ModifiedAt = now;
+      referral.ModifiedByUser = GetSystemAdminUser();
+      referral.PatientId = 1;
+      referral.ReferralStatusId = 1;
+
+      // referral with a previous examination
+
+      if ((referral =
+      _context.Referrals
+                .SingleOrDefault(g => g.PatientId == 2)) == null)
+      {
+        referral = new Referral();
+        _context.Add(referral);
+      }
+
+      referral.CreatedAt = now;
+      referral.CreatedByUser = GetSystemAdminUser();
+      referral.IsActive = true;
+      referral.IsPlannedExamination = true;
+      referral.LeadAmhpUser = GetSystemAdminUser();
+      referral.ModifiedAt = now;
+      referral.ModifiedByUser = GetSystemAdminUser();
       referral.PatientId = 2;
       referral.ReferralStatusId = 1;
-      referral.IsPlannedExamination = true;
-      referral.LeadAmhpUser = GetSystemAdminUser();
 
-            if ((referral =
+      // referral with no examinations
+
+      if ((referral =
       _context.Referrals
-                .SingleOrDefault(g => g.PatientId == 3 )) == null)
+                .SingleOrDefault(g => g.PatientId == 3)) == null)
       {
         referral = new Referral();
         _context.Add(referral);
       }
 
-      referral.IsActive = true;
-      referral.ModifiedAt = now;
-      referral.ModifiedByUser = GetSystemAdminUser();
       referral.CreatedAt = now;
       referral.CreatedByUser = GetSystemAdminUser();
+      referral.IsActive = true;
+      referral.IsPlannedExamination = true;
+      referral.LeadAmhpUser = GetSystemAdminUser();
+      referral.ModifiedAt = now;
+      referral.ModifiedByUser = GetSystemAdminUser();
       referral.PatientId = 3;
       referral.ReferralStatusId = 1;
-      referral.IsPlannedExamination = true;
-      referral.LeadAmhpUser = GetSystemAdminUser();
 
-            if ((referral =
+      // referral with both current and previous examinations
+
+      if ((referral =
       _context.Referrals
-                .SingleOrDefault(g => g.PatientId == 4 )) == null)
+                .SingleOrDefault(g => g.PatientId == 4)) == null)
       {
         referral = new Referral();
         _context.Add(referral);
       }
 
-      referral.IsActive = true;
-      referral.ModifiedAt = now;
-      referral.ModifiedByUser = GetSystemAdminUser();
       referral.CreatedAt = now;
       referral.CreatedByUser = GetSystemAdminUser();
+      referral.IsActive = true;
+      referral.IsPlannedExamination = true;
+      referral.LeadAmhpUser = GetSystemAdminUser();
+      referral.ModifiedAt = now;
+      referral.ModifiedByUser = GetSystemAdminUser();
       referral.PatientId = 4;
       referral.ReferralStatusId = 1;
-      referral.IsPlannedExamination = true;
-      referral.LeadAmhpUser = GetSystemAdminUser();
 
-            if ((referral =
+      // referral with current examination and allocated doctors
+
+      if ((referral =
       _context.Referrals
-                .SingleOrDefault(g => g.PatientId == 5 )) == null)
+                .SingleOrDefault(g => g.PatientId == 5)) == null)
       {
         referral = new Referral();
         _context.Add(referral);
       }
 
-      referral.IsActive = true;
-      referral.ModifiedAt = now;
-      referral.ModifiedByUser = GetSystemAdminUser();
       referral.CreatedAt = now;
       referral.CreatedByUser = GetSystemAdminUser();
-      referral.PatientId = 5;
-      referral.ReferralStatusId = 1;
+      referral.IsActive = true;
       referral.IsPlannedExamination = true;
       referral.LeadAmhpUser = GetSystemAdminUser();
+      referral.ModifiedAt = now;
+      referral.ModifiedByUser = GetSystemAdminUser();
+      referral.PatientId = 5;
+      referral.ReferralStatusId = 1;
+
+      // referral with current examination and notification responses
+
+      if ((referral =
+      _context.Referrals
+                .SingleOrDefault(g => g.PatientId == 6)) == null)
+      {
+        referral = new Referral();
+        _context.Add(referral);
+      }
+
+      referral.CreatedAt = now;
+      referral.CreatedByUser = GetSystemAdminUser();
+      referral.IsActive = true;
+      referral.IsPlannedExamination = true;
+      referral.LeadAmhpUser = GetSystemAdminUser();
+      referral.ModifiedAt = now;
+      referral.ModifiedByUser = GetSystemAdminUser();
+      referral.PatientId = 6;
+      referral.ReferralStatusId = 1;
+
+      // referral with current examination and notification responses and allocated doctors
+
+      if ((referral =
+      _context.Referrals
+                .SingleOrDefault(g => g.PatientId == 7)) == null)
+      {
+        referral = new Referral();
+        _context.Add(referral);
+      }
+
+      referral.CreatedAt = now;
+      referral.CreatedByUser = GetSystemAdminUser();
+      referral.IsActive = true;
+      referral.IsPlannedExamination = true;
+      referral.LeadAmhpUser = GetSystemAdminUser();
+      referral.ModifiedAt = now;
+      referral.ModifiedByUser = GetSystemAdminUser();
+      referral.PatientId = 7;
+      referral.ReferralStatusId = 1;
     }
   }
 }
