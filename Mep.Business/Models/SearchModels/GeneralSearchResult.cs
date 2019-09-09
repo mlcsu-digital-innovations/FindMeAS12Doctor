548fddd3654a9f52461dc27cd9fcdc365f35f04d
Mark Carter <mark.carter4@nhs.net>
2019-09-09 12:18:04 +0100

    #9966 - Create a business model for the results of a general search query

--- Mep.Business/Models/SearchModels/GeneralSearchResult.cs
+++ Mep.Business/Models/SearchModels/GeneralSearchResult.cs
@@ -3,6 +3,6 @@ namespace Mep.Business.Models.SearchModels
   public class GeneralSearchResult
   {
     public int Id { get; set; }
-    public string DisplayText { get; set; }
+    public string ResultText { get; set; }
   }
 }
