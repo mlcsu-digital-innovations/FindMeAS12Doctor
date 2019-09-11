fb466889facd2bd6cd7f1c32210bf119c27932a2
Mark Carter <mark.carter4@nhs.net>
2019-09-11 12:52:49 +0100

    Removed redundant test

--- Mep.Ui/src/app/app.component.spec.ts
+++ Mep.Ui/src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Mep');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Mep app is running!');
-  });
 });
