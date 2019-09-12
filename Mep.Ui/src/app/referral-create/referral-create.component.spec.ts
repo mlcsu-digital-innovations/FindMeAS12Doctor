0daca664c0ca43163b6a7f482335dd345c5ab681
Mark Carter <mark.carter4@nhs.net>
2019-09-12 10:19:44 +0100

    Corrected test for ReferralComponent

--- Mep.Ui/src/app/referral-create/referral-create.component.spec.ts
+++ Mep.Ui/src/app/referral-create/referral-create.component.spec.ts
@@ -3,6 +3,7 @@ import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { ReferralCreateComponent } from './referral-create.component';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { DisableControlDirective } from '../directives/disable-control/disable-control.directive';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 describe('ReferralCreateComponent', () => {
   let component: ReferralCreateComponent;
@@ -11,7 +12,7 @@ describe('ReferralCreateComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ReferralCreateComponent, NavbarComponent, DisableControlDirective ],
-      imports: [ReactiveFormsModule]
+      imports: [ReactiveFormsModule, NgbModule]
     })
     .compileComponents();
   }));
