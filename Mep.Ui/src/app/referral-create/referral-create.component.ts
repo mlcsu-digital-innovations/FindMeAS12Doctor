33f1c56c24cfe9caf019cce2dc9ba826a9fd1c18
Mark Carter <mark.carter4@nhs.net>
2019-09-12 10:08:25 +0100

    #9946 - Create a button that when clicked will validate either NHS Number or Alternative Identifier

--- Mep.Ui/src/app/referral-create/referral-create.component.ts
+++ Mep.Ui/src/app/referral-create/referral-create.component.ts
@@ -1,24 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { NhsNumberValidFormat } from '../helpers/nhs-number.validator';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { NhsNumberValidFormat } from "../helpers/nhs-number.validator";
+import {  } from '@angular/router'
 
 @Component({
-  selector: 'app-referral-create',
-  templateUrl: './referral-create.component.html',
-  styleUrls: ['./referral-create.component.css']
+  selector: "app-referral-create",
+  templateUrl: "./referral-create.component.html",
+  styleUrls: ["./referral-create.component.css"]
 })
 export class ReferralCreateComponent implements OnInit {
-
   myForm: FormGroup;
   patientForm: FormGroup;
   value = false;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.patientForm = this.formBuilder.group({
-      nhsNumber: ['', [Validators.maxLength(10), Validators.pattern('^[1-9]\\d{9}$'), NhsNumberValidFormat]],
-      alternativeIdentifier: ['', [Validators.maxLength(200), Validators.pattern('.*[0-9].*')]]
+      nhsNumber: [
+        "",
+        [
+          Validators.maxLength(10),
+          Validators.pattern("^[1-9]\\d{9}$"),
+          NhsNumberValidFormat
+        ]
+      ],
+      alternativeIdentifier: [
+        "",
+        [Validators.maxLength(200), Validators.pattern(".*[0-9].*")]
+      ]
     });
   }
 
@@ -39,37 +49,55 @@ export class ReferralCreateComponent implements OnInit {
   }
 
   HasInvalidNHSNumber(): boolean {
-    return this.nhsNumberField.value !== '' && this.nhsNumberField.errors !== null;
+    return (
+      this.nhsNumberField.value !== "" && this.nhsNumberField.errors !== null
+    );
   }
 
   HasValidNHSNumber(): boolean {
-    return this.nhsNumberField.value !== '' && this.nhsNumberField.errors == null;
+    return (
+      this.nhsNumberField.value !== "" && this.nhsNumberField.errors == null
+    );
   }
 
   HasInvalidAlternativeIdentifier(): boolean {
-    return this.alternativeIdentifierField.value !== '' && this.alternativeIdentifierField.errors !== null;
+    return (
+      this.alternativeIdentifierField.value !== "" &&
+      this.alternativeIdentifierField.errors !== null
+    );
   }
 
   HasValidAlternativeIdentifier(): boolean {
-    return this.alternativeIdentifierField.value !== '' && this.alternativeIdentifierField.errors == null;
+    return (
+      this.alternativeIdentifierField.value !== "" &&
+      this.alternativeIdentifierField.errors == null
+    );
   }
 
   DisableIfFieldHasValue(fieldName: string): boolean {
-
     if (fieldName in this.patientForm.controls) {
-      return this.patientForm.controls[fieldName].value !== '';
+      return this.patientForm.controls[fieldName].value !== "";
     } else {
-      throw new Error(`DisableIfFieldHasValue(fieldName: string) unable to find field [${fieldName}]`);
+      throw new Error(
+        `DisableIfFieldHasValue(fieldName: string) unable to find field [${fieldName}]`
+      );
     }
   }
 
   DisablePatientValidationButtonIfFieldsAreInvalid(): boolean {
-
     // field is only valid if it has a value and there aren't any errors
-    const nhsNumberFieldInValid = this.nhsNumberField.value === '' || this.nhsNumberField.errors !== null;
+    const nhsNumberFieldInValid =
+      this.nhsNumberField.value === "" || this.nhsNumberField.errors !== null;
     const alternativeIdentifierFieldInValid =
-      this.alternativeIdentifierField.value === '' || this.alternativeIdentifierField.errors !== null;
+      this.alternativeIdentifierField.value === "" ||
+      this.alternativeIdentifierField.errors !== null;
 
     return nhsNumberFieldInValid && alternativeIdentifierFieldInValid;
+  }
+
+  ValidatePatient(): void {
+    console.log("Validating patient ...");
+
+    const search = { NhsNumber: 8156079965 };
   }
 }
