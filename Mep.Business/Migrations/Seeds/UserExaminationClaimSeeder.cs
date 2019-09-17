a0c45417e3e1b8994352d3769112f3db36a6a0c6
Stephen Harland <stephen.harland1@nhs.net>
2019-09-17 11:57:13 +0100

    updated UserExaminationClaimSeeder

---
+++ Mep.Business/Migrations/Seeds/UserExaminationClaimSeeder.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+using Mep.Data.Entities;
+
+namespace Mep.Business.Migrations.Seeds
+{
+  internal class UserExaminationClaimSeeder : SeederBase
+  {
+
+    internal UserExaminationClaimSeeder(ApplicationContext context)
+      : base(context)
+    {
+    }
+
+    internal void SeedData()
+    {
+    }
+  }
+}
