87d224fb0ba27ea06ec0fd4548d14f948da348f6
Stephen Harland <stephen.harland1@nhs.net>
2019-09-23 10:46:13 +0100

    added DateTimeOffset.Now to seederbase

--- Mep.Business/Migrations/Seeds/SeederBase.cs
+++ Mep.Business/Migrations/Seeds/SeederBase.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Mep.Data.Entities;
 
@@ -5,6 +6,8 @@ namespace Mep.Business.Migrations.Seeds
 {
   public class SeederBase
   {
+    public DateTimeOffset _now = DateTimeOffset.Now;
+    
     protected const string SystemAdminIdentityServerIdentifier = 
       "bf673270-2538-4e59-9d26-5b4808fd9ef6";
     protected readonly ApplicationContext _context;
