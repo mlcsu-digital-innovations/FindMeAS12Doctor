5a7b5396f8e8a710b2725ddff227573daea1d1a3
Stephen Harland <stephen.harland1@nhs.net>
2019-09-25 11:18:52 +0100

    added GetFirstCcg() to examination seeder

--- Mep.Business/Migrations/Seeds/ExaminationSeeder.cs
+++ Mep.Business/Migrations/Seeds/ExaminationSeeder.cs
@@ -14,24 +14,24 @@ namespace Mep.Business.Migrations.Seeds
     {
       Examination examination;
 
-      Referral referral1 = _context.Referrals.Where(referral => referral.PatientId == _patient1.Id).FirstOrDefault();
-      Referral referral2 = _context.Referrals.Where(referral => referral.PatientId == _patient2.Id).FirstOrDefault();
-      Referral referral3 = _context.Referrals.Where(referral => referral.PatientId == _patient3.Id).FirstOrDefault();
-      Referral referral4 = _context.Referrals.Where(referral => referral.PatientId == _patient4.Id).FirstOrDefault();
-      Referral referral5 = _context.Referrals.Where(referral => referral.PatientId == _patient5.Id).FirstOrDefault();
-      Referral referral6 = _context.Referrals.Where(referral => referral.PatientId == _patient6.Id).FirstOrDefault();
-      Referral referral7 = _context.Referrals.Where(referral => referral.PatientId == _patient7.Id).FirstOrDefault();
+      Referral referral1 = _context.Referrals.Single(referral => referral.PatientId == GetPatientIdByNhsNumber(PATIENTNHSNUMBER1));
+      Referral referral2 = _context.Referrals.Single(referral => referral.PatientId == GetPatientIdByNhsNumber(PATIENTNHSNUMBER2));
+      Referral referral3 = _context.Referrals.Single(referral => referral.PatientId == GetPatientIdByNhsNumber(PATIENTNHSNUMBER3));
+      Referral referral4 = _context.Referrals.Single(referral => referral.PatientId == GetPatientIdByNhsNumber(PATIENTNHSNUMBER4));
+      Referral referral5 = _context.Referrals.Single(referral => referral.PatientId == GetPatientIdByAlternativeIdentifier(PATIENTALTERNATIVEIDENTIFIER5));
+      Referral referral6 = _context.Referrals.Single(referral => referral.PatientId == GetPatientIdByAlternativeIdentifier(PATIENTALTERNATIVEIDENTIFIER6));
+      Referral referral7 = _context.Referrals.Single(referral => referral.PatientId == GetPatientIdByAlternativeIdentifier(PATIENTALTERNATIVEIDENTIFIER7));
 
       // examination for referral with a current examination with no allocated doctors or notification responses
 
       if ((examination =
         _context.Examinations
-          .SingleOrDefault(g => g.Address1 == "Examination Address 1")) == null)
+          .SingleOrDefault(g => g.Address1 == EXAMINATIONADDRESS1)) == null)
       {
         examination = new Examination();
         _context.Add(examination);
       }
-      examination.Address1 = "Examination Address 1";
+      examination.Address1 = EXAMINATIONADDRESS1;
       examination.CcgId = 1;
       examination.CreatedByUser = GetSystemAdminUser();
       examination.IsActive = true;
@@ -39,18 +39,18 @@ namespace Mep.Business.Migrations.Seeds
       examination.ModifiedByUser = GetSystemAdminUser();
       examination.Postcode = "ST14 5PP";
       examination.ReferralId = referral1.Id;
-      examination.SpecialityId = 1;
+      examination.SpecialityId = GetSpecialityId();
 
       // examination for referral with a previous examination
 
       if ((examination =
         _context.Examinations
-          .SingleOrDefault(g => g.Address1 == "Examination Address 2")) == null)
+          .SingleOrDefault(g => g.Address1 == EXAMINATIONADDRESS2)) == null)
       {
         examination = new Examination();
         _context.Add(examination);
       }
-      examination.Address1 = "Examination Address 2";
+      examination.Address1 = EXAMINATIONADDRESS2;
       examination.CcgId = 1;
       examination.CompletedByUser = GetSystemAdminUser();
       examination.CompletedTime = _now;
@@ -60,19 +60,19 @@ namespace Mep.Business.Migrations.Seeds
       examination.ModifiedByUser = GetSystemAdminUser();
       examination.Postcode = "ST2 9QL";
       examination.ReferralId = referral2.Id;
-      examination.SpecialityId = 1;
+      examination.SpecialityId = GetSpecialityId();
 
       // examinations for referral with both current and previous examinations
 
       if ((examination =
         _context.Examinations
-          .SingleOrDefault(g => g.Address1 == "Examination Address 3")) == null)
+          .SingleOrDefault(g => g.Address1 == EXAMINATIONADDRESS3)) == null)
       {
         examination = new Examination();
         _context.Add(examination);
       }
-      examination.Address1 = "Examination Address 3";
-      examination.CcgId = 1;
+      examination.Address1 = EXAMINATIONADDRESS3;
+      examination.CcgId = GetFirstCcg();
       examination.CompletedByUser = GetSystemAdminUser();
       examination.CompletedTime = _now;
       examination.CreatedByUser = GetSystemAdminUser();
@@ -80,82 +80,82 @@ namespace Mep.Business.Migrations.Seeds
       examination.ModifiedAt = _now;
       examination.ModifiedByUser = GetSystemAdminUser();
       examination.Postcode = "ST4 8ZH";
-      examination.ReferralId = referral4.Id;;
-      examination.SpecialityId = 1;
+      examination.ReferralId = referral4.Id;
+      examination.SpecialityId = GetSpecialityId();
 
       if ((examination =
         _context.Examinations
-          .SingleOrDefault(g => g.Address1 == "Examination Address 4")) == null)
+          .SingleOrDefault(g => g.Address1 == EXAMINATIONADDRESS4)) == null)
       {
         examination = new Examination();
         _context.Add(examination);
       }
-      examination.Address1 = "Examination Address 4";
-      examination.CcgId = 1;
+      examination.Address1 = EXAMINATIONADDRESS4;
+      examination.CcgId = GetFirstCcg();
       examination.CreatedByUser = GetSystemAdminUser();
       examination.IsActive = true;
       examination.ModifiedAt = _now;
       examination.ModifiedByUser = GetSystemAdminUser();
       examination.Postcode = "ST5 7UE";
-      examination.ReferralId = referral4.Id;;
-      examination.SpecialityId = 1;
+      examination.ReferralId = referral4.Id;
+      examination.SpecialityId = GetSpecialityId();
 
       // examination for referral with current examination and allocated doctors
 
       if ((examination =
         _context.Examinations
-          .SingleOrDefault(g => g.Address1 == "Examination Address 5")) == null)
+          .SingleOrDefault(g => g.Address1 == EXAMINATIONADDRESS5)) == null)
       {
         examination = new Examination();
         _context.Add(examination);
       }
-      examination.Address1 = "Examination Address 5";
-      examination.CcgId = 1;
+      examination.Address1 = EXAMINATIONADDRESS5;
+      examination.CcgId = GetFirstCcg();
       examination.CreatedByUser = GetSystemAdminUser();
       examination.IsActive = true;
       examination.ModifiedAt = _now;
       examination.ModifiedByUser = GetSystemAdminUser();
       examination.Postcode = "ST10 3JH";
-      examination.ReferralId = referral5.Id;;
-      examination.SpecialityId = 1;
+      examination.ReferralId = referral5.Id;
+      examination.SpecialityId = GetSpecialityId();
 
       // examination for referral with current examination and notification responses
 
       if ((examination =
         _context.Examinations
-          .SingleOrDefault(g => g.Address1 == "Examination Address 6")) == null)
+          .SingleOrDefault(g => g.Address1 == EXAMINATIONADDRESS6)) == null)
       {
         examination = new Examination();
         _context.Add(examination);
       }
-      examination.Address1 = "Examination Address 6";
-      examination.CcgId = 1;
+      examination.Address1 = EXAMINATIONADDRESS6;
+      examination.CcgId = GetFirstCcg();
       examination.CreatedByUser = GetSystemAdminUser();
       examination.IsActive = true;
       examination.ModifiedAt = _now;
       examination.ModifiedByUser = GetSystemAdminUser();
       examination.Postcode = "ST5 3DX";
-      examination.ReferralId = referral6.Id;;
-      examination.SpecialityId = 1;
+      examination.ReferralId = referral6.Id;
+      examination.SpecialityId = GetSpecialityId();
 
       // examination for referral with current examination and notification responses and allocated doctors
 
       if ((examination =
         _context.Examinations
-          .SingleOrDefault(g => g.Address1 == "Examination Address 7")) == null)
+          .SingleOrDefault(g => g.Address1 == EXAMINATIONADDRESS7)) == null)
       {
         examination = new Examination();
         _context.Add(examination);
       }
-      examination.Address1 = "Examination Address 7";
-      examination.CcgId = 1;
+      examination.Address1 = EXAMINATIONADDRESS7;
+      examination.CcgId = GetFirstCcg();
       examination.CreatedByUser = GetSystemAdminUser();
       examination.IsActive = true;
       examination.ModifiedAt = _now;
       examination.ModifiedByUser = GetSystemAdminUser();
       examination.Postcode = "ST6 7HG";
-      examination.ReferralId = referral7.Id;;
-      examination.SpecialityId = 1;
+      examination.ReferralId = referral7.Id;
+      examination.SpecialityId = GetSpecialityId();
     }
   }
 }
--- Mep.Business/Migrations/Seeds/SeederBase.cs
+++ Mep.Business/Migrations/Seeds/SeederBase.cs
@@ -7,8 +7,6 @@ namespace Mep.Business.Migrations.Seeds
   public class SeederBase
   {
     protected DateTimeOffset _now = DateTimeOffset.Now;
-    protected GenderType _femaleGender;
-    protected GenderType _maleGender;
     protected Patient _patient1;
     protected Patient _patient2;
     protected Patient _patient3;
@@ -19,10 +17,7 @@ namespace Mep.Business.Migrations.Seeds
     protected ReferralStatus _referralStatus;
     protected Speciality _speciality;
 
-    protected const string SystemAdminIdentityServerIdentifier = "bf673270-2538-4e59-9d26-5b4808fd9ef6";
-    protected const string FEMALE = "Female";
-    protected const string MALE = "Male";
-    protected const string OTHER = "Other";
+
     protected const string EXAMINATIONADDRESS1 = "Examination Address 1";
     protected const string EXAMINATIONADDRESS2 = "Examination Address 2";
     protected const string EXAMINATIONADDRESS3 = "Examination Address 3";
@@ -30,11 +25,11 @@ namespace Mep.Business.Migrations.Seeds
     protected const string EXAMINATIONADDRESS5 = "Examination Address 5";
     protected const string EXAMINATIONADDRESS6 = "Examination Address 6";
     protected const string EXAMINATIONADDRESS7 = "Examination Address 7";
-    
-    protected const long PATIENTNHSNUMBER1 = 9486844275;
-    protected const long PATIENTNHSNUMBER2 = 9657966272;
-    protected const long PATIENTNHSNUMBER3 = 9070304333;
-    protected const long PATIENTNHSNUMBER4 = 9813607416;
+    protected const string FEMALE = "Female";
+    protected const string MALE = "Male";
+    protected const string NOTIFICATIONTEXT1 = "Notification Text 1";
+    protected const string NOTIFICATIONTEXT2 = "Notification Text 2";
+    protected const string OTHER = "Other";
     protected const string PATIENTALTERNATIVEIDENTIFIER1 = "Test Patient #1";
     protected const string PATIENTALTERNATIVEIDENTIFIER2 = "Test Patient #2";
     protected const string PATIENTALTERNATIVEIDENTIFIER3 = "Test Patient #3";
@@ -43,58 +38,139 @@ namespace Mep.Business.Migrations.Seeds
     protected const string PATIENTALTERNATIVEIDENTIFIER6 = "Test Patient #6";
     protected const string PATIENTALTERNATIVEIDENTIFIER7 = "Test Patient #7";
     protected const string PATIENTALTERNATIVEIDENTIFIER8 = "Test Patient #8";
+    protected const long PATIENTNHSNUMBER1 = 9486844275;
+    protected const long PATIENTNHSNUMBER2 = 9657966272;
+    protected const long PATIENTNHSNUMBER3 = 9070304333;
+    protected const long PATIENTNHSNUMBER4 = 9813607416;
     protected const string REFERRALSTATUS = "New Referral";
     protected const string SPECIALITY = "Section 12";
-    protected const string NOTIFICATIONTEXT1 = "Notification Text 1";
-    protected const string NOTIFICATIONTEXT2 = "Notification Text 2";
-    
+    protected const string USERDISPLAYNAMEFEMALE = "Doctor Female";
+    protected const string USERDISPLAYNAMEMALE = "Doctor Male";
+
+    protected const string SystemAdminIdentityServerIdentifier = "bf673270-2538-4e59-9d26-5b4808fd9ef6";
+
 
     protected readonly ApplicationContext _context;
 
+    protected int GetFirstCcg()
+    {
+      try
+      {
+        return _context.Ccgs.FirstOrDefault().Id;
+      }
+      catch (Exception ex)
+      {
+        throw new Exception("Cannot find CCG", ex);
+      }
+    }
+
     protected int GetFemaleGenderTypeId()
     {
-      try {
+      try
+      {
         return _context.GenderTypes.Single(gender => gender.Name == FEMALE).Id;
-      } catch (Exception ex) {
+      }
+      catch (Exception ex)
+      {
         throw new Exception("Cannot find female in GenderTypes", ex);
       }
     }
 
     protected int GetMaleGenderTypeId()
     {
-      try {
+      try
+      {
         return _context.GenderTypes.Single(gender => gender.Name == MALE).Id;
-      } catch (Exception ex) {
+      }
+      catch (Exception ex)
+      {
         throw new Exception("Cannot find male in GenderTypes", ex);
       }
     }
 
     protected int GetOtherGenderTypeId()
     {
-      try {
+      try
+      {
         return _context.GenderTypes.Single(gender => gender.Name == OTHER).Id;
-      } catch (Exception ex) {
+      }
+      catch (Exception ex)
+      {
         throw new Exception("Cannot find other in GenderTypes", ex);
       }
     }
 
     protected int GetReferralStatusId()
     {
-      try {
-        return _context.GenderTypes.Single(gender => gender.Name == REFERRALSTATUS).Id;
-      } catch (Exception ex) {
+      try
+      {
+        return _context.ReferralStatuses.Single(referralStatus => referralStatus.Name == REFERRALSTATUS).Id;
+      }
+      catch (Exception ex)
+      {
         throw new Exception("Cannot find New Referral in ReferralStatuses", ex);
       }
     }
 
-    protected int GetPatientIdByNhsNumber(long nhsNumber) {
-      try {
+    protected int GetSpecialityId()
+    {
+      try
+      {
+        return _context.Specialities.Single(speciality => speciality.Name == SPECIALITY).Id;
+      }
+      catch (Exception ex)
+      {
+        throw new Exception("Cannot find Section 12 in Speciality", ex);
+      }
+    }
+
+    protected int GetPatientIdByNhsNumber(long nhsNumber)
+    {
+      try
+      {
         return _context.Patients.Single(patient => patient.NhsNumber == nhsNumber).Id;
-      } catch (Exception ex) {
+      }
+      catch (Exception ex)
+      {
         throw new Exception($"Cannot find patinet with an NHS Number of {nhsNumber} in Patients", ex);
       }
     }
 
+    protected int GetNotificationTextId(string notificationTextName)
+    {
+      try
+      {
+        return _context.NotificationTexts.Single(notificationText => notificationText.Name == notificationTextName).Id;
+      }
+      catch (Exception ex)
+      {
+        throw new Exception($"Cannot find {notificationTextName} in NotificationTexts", ex);
+      }
+    }
+
+    protected int GetPatientIdByAlternativeIdentifier(string alternativeIdentifier)
+    {
+      try
+      {
+        return _context.Patients.Single(patient => patient.AlternativeIdentifier == alternativeIdentifier).Id;
+      }
+      catch (Exception ex)
+      {
+        throw new Exception($"Cannot find patinet with an NHS Number of {alternativeIdentifier} in Patients", ex);
+      }
+    }
+
+    protected int GetUserIdByDisplayname(string displayName)
+    {
+      try
+      {
+        return _context.Users.Single(user => user.DisplayName == displayName).Id;
+      }
+      catch (Exception ex)
+      {
+        throw new Exception($"Cannot find user {displayName} in Users", ex);
+      }
+    }
 
     public SeederBase(ApplicationContext context)
     {
