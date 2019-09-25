ddc08ca338fac228fe5162cc097d1bdb2dca447d
Stephen Harland <stephen.harland1@nhs.net>
2019-09-25 11:46:28 +0100

    added test data to organisation seeder

--- Mep.Business/Migrations/Seeds/OrganisationSeeder.cs
+++ Mep.Business/Migrations/Seeds/OrganisationSeeder.cs
@@ -13,6 +13,59 @@ namespace Mep.Business.Migrations.Seeds
 
     internal void SeedData()
     {
+      Organisation organisation;
+
+      if ((organisation =
+        _context.Organisations
+          .SingleOrDefault(u => u.Name == ORGANISATION1)) == null)
+      {
+        organisation = new Organisation();
+        _context.Add(organisation);
+      }
+      organisation.Description = ORGANISATION1;
+      organisation.IsActive = true;
+      organisation.ModifiedAt = _now;
+      organisation.ModifiedByUser = GetSystemAdminUser();
+      organisation.Name = ORGANISATION1;
+
+            if ((organisation =
+        _context.Organisations
+          .SingleOrDefault(u => u.Name == ORGANISATION2)) == null)
+      {
+        organisation = new Organisation();
+        _context.Add(organisation);
+      }
+      organisation.Description = ORGANISATION2;
+      organisation.IsActive = true;
+      organisation.ModifiedAt = _now;
+      organisation.ModifiedByUser = GetSystemAdminUser();
+      organisation.Name = ORGANISATION2;
+
+      if ((organisation =
+        _context.Organisations
+          .SingleOrDefault(u => u.Name == ORGANISATION3)) == null)
+      {
+        organisation = new Organisation();
+        _context.Add(organisation);
+      }
+      organisation.Description = ORGANISATION3;
+      organisation.IsActive = true;
+      organisation.ModifiedAt = _now;
+      organisation.ModifiedByUser = GetSystemAdminUser();
+      organisation.Name = ORGANISATION3;
+
+      if ((organisation =
+        _context.Organisations
+          .SingleOrDefault(u => u.Name == ORGANISATION4)) == null)
+      {
+        organisation = new Organisation();
+        _context.Add(organisation);
+      }
+      organisation.Description = ORGANISATION4;
+      organisation.IsActive = true;
+      organisation.ModifiedAt = _now;
+      organisation.ModifiedByUser = GetSystemAdminUser();
+      organisation.Name = ORGANISATION4;
     }
   }
 }
