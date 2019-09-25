0c5d0b63e068b8ff4eaf79b6492f9a0775382f6e
Stephen Harland <stephen.harland1@nhs.net>
2019-09-25 11:46:01 +0100

    added organisation users to user seeder

--- Mep.Business/Migrations/Seeds/UserSeeder.cs
+++ Mep.Business/Migrations/Seeds/UserSeeder.cs
@@ -53,85 +53,78 @@ namespace Mep.Business.Migrations.Seeds
       user.Section12ApprovalStatusId = null;
       user.Section12ExpiryDate = null;
 
-
-
-
-
-
-
-
       if ((user =
         _context.Users
-          .SingleOrDefault(g => g.DisplayName == USERDISPLAYNAMEMALE)) == null)
+          .SingleOrDefault(g => g.DisplayName == ORGANISATION1USER)) == null)
       {
         user = new User();
         _context.Add(user);
       }
-      user.DisplayName = USERDISPLAYNAMEMALE;
+      user.DisplayName = ORGANISATION1USER;
       user.GenderTypeId = GetMaleGenderTypeId();
       user.GmcNumber = null;
       user.HasReadTermsAndConditions = true;
       user.IdentityServerIdentifier = Guid.NewGuid().ToString();
       user.IsActive = true;
       user.ModifiedByUser = GetSystemAdminUser();
-      user.OrganisationId = 1;
+      user.OrganisationId = GetOrganisationIdByName(ORGANISATION1);
       user.ProfileTypeId = 1;
       user.Section12ApprovalStatusId = null;
       user.Section12ExpiryDate = null;
             
       if ((user =
         _context.Users
-          .SingleOrDefault(g => g.DisplayName == USERDISPLAYNAMEMALE)) == null)
+          .SingleOrDefault(g => g.DisplayName == ORGANISATION2USER)) == null)
       {
         user = new User();
         _context.Add(user);
       }
-      user.DisplayName = USERDISPLAYNAMEMALE;
+      user.DisplayName = ORGANISATION2USER;
       user.GenderTypeId = GetMaleGenderTypeId();
       user.GmcNumber = null;
       user.HasReadTermsAndConditions = true;
       user.IdentityServerIdentifier = Guid.NewGuid().ToString();
       user.IsActive = true;
       user.ModifiedByUser = GetSystemAdminUser();
-      user.OrganisationId = 1;
+      user.OrganisationId = GetOrganisationIdByName(ORGANISATION2);
       user.ProfileTypeId = 1;
       user.Section12ApprovalStatusId = null;
       user.Section12ExpiryDate = null;
             
       if ((user =
         _context.Users
-          .SingleOrDefault(g => g.DisplayName == USERDISPLAYNAMEMALE)) == null)
+          .SingleOrDefault(g => g.DisplayName == ORGANISATION3USER)) == null)
       {
         user = new User();
         _context.Add(user);
       }
-      user.DisplayName = USERDISPLAYNAMEMALE;
+      user.DisplayName = ORGANISATION3USER;
       user.GenderTypeId = GetMaleGenderTypeId();
       user.GmcNumber = null;
       user.HasReadTermsAndConditions = true;
       user.IdentityServerIdentifier = Guid.NewGuid().ToString();
       user.IsActive = true;
       user.ModifiedByUser = GetSystemAdminUser();
-      user.OrganisationId = 1;
+      user.OrganisationId = GetOrganisationIdByName(ORGANISATION3);
       user.ProfileTypeId = 1;
       user.Section12ApprovalStatusId = null;
       user.Section12ExpiryDate = null;
             
       if ((user =
         _context.Users
-          .SingleOrDefault(g => g.DisplayName == USERDISPLAYNAMEMALE)) == null)
+          .SingleOrDefault(g => g.DisplayName == ORGANISATION4USER)) == null)
       {
         user = new User();
         _context.Add(user);
       }
-      user.DisplayName = USERDISPLAYNAMEMALE;
+      user.DisplayName = ORGANISATION4USER;
       user.GenderTypeId = GetMaleGenderTypeId();
       user.GmcNumber = null;
       user.HasReadTermsAndConditions = true;
       user.IdentityServerIdentifier = Guid.NewGuid().ToString();
       user.IsActive = true;
       user.ModifiedByUser = GetSystemAdminUser();
-      user.OrganisationId = 1;
+      user.OrganisationId = GetOrganisationIdByName(ORGANISATION3);
       user.ProfileTypeId = 1;
       user.Section12ApprovalStatusId = null;
       user.Section12ExpiryDate = null;
