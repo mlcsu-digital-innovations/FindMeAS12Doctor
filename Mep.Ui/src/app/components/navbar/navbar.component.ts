f34a58ed3d3ce15dc62ca7305a9afc32a9fadfee
Mark Carter <mark.carter4@nhs.net>
2019-09-25 15:15:44 +0100

    Components moved to new folder

---
+++ Mep.Ui/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  // Add standard navbar options here !
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Mep.Ui/src/app/components/referral/referral-create/referral-create.component.spec.ts
@@ -0,0 +1,40 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DisableControlDirective } from '../../../directives/disable-control/disable-control.directive';
+import { NavbarComponent } from '../../navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToastsComponent } from '../../toasts/toasts.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ReferralCreateComponent } from './referral-create.component';
+
+describe('ReferralCreateComponent', () => {
+  let component: ReferralCreateComponent;
+  let fixture: ComponentFixture<ReferralCreateComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        DisableControlDirective,
+        NavbarComponent,
+        ReferralCreateComponent,
+        ToastsComponent
+    ],
+      imports: [
+        HttpClientModule,
+        NgbModule,
+        ReactiveFormsModule
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReferralCreateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
