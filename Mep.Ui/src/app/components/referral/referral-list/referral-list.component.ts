6129b7d503245f49404fd7c5201b83e61f461d2a
Mark Carter <mark.carter4@nhs.net>
2019-09-25 15:14:47 +0100

    Dummy referral-list page

---
+++ Mep.Ui/src/app/components/referral/referral-list/referral-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-referral-list',
+  templateUrl: './referral-list.component.html',
+  styleUrls: ['./referral-list.component.css']
+})
+export class ReferralListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
