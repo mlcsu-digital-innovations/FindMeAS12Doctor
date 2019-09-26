3d94377651f455aeb26bc846acabf8fe2bccdea2
Stephen Harland <stephen.harland1@nhs.net>
2019-09-26 11:52:53 +0100

    created profiletypeseeder

--- Mep.Business/Migrations/Seeds/ProfileTypeSeeder.cs
+++ Mep.Business/Migrations/Seeds/ProfileTypeSeeder.cs
@@ -13,6 +13,50 @@ namespace Mep.Business.Migrations.Seeds
 
     internal void SeedData()
     {
+      ProfileType profileType;
+
+      if (
+        (profileType = _context.ProfileTypes.SingleOrDefault(u => u.Name == PROFILETYPE1)) == null)
+
+      {
+        profileType = new ProfileType();
+        _context.Add(profileType);
+      }
+
+      profileType.Description = PROFILETYPE1;
+      profileType.IsActive = true;
+      profileType.ModifiedAt = _now;
+      profileType.ModifiedByUser = GetSystemAdminUser();
+      profileType.Name = PROFILETYPE1;
+
+      if (
+        (profileType = _context.ProfileTypes.SingleOrDefault(u => u.Name == PROFILETYPE2)) == null)
+
+      {
+        profileType = new ProfileType();
+        _context.Add(profileType);
+      }
+
+      profileType.Description = PROFILETYPE2;
+      profileType.IsActive = true;
+      profileType.ModifiedAt = _now;
+      profileType.ModifiedByUser = GetSystemAdminUser();
+      profileType.Name = PROFILETYPE2;
+
+      if (
+        (profileType = _context.ProfileTypes.SingleOrDefault(u => u.Name == PROFILETYPE3)) == null)
+
+      {
+        profileType = new ProfileType();
+        _context.Add(profileType);
+      }
+
+      profileType.Description = PROFILETYPE3;
+      profileType.IsActive = true;
+      profileType.ModifiedAt = _now;
+      profileType.ModifiedByUser = GetSystemAdminUser();
+      profileType.Name = PROFILETYPE3;
+
     }
   }
 }
