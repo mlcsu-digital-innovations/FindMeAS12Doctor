9911691579789cbf853b9d3a3c8afc85b8ea35cf
Mark Carter <mark.carter4@nhs.net>
2019-09-27 14:10:26 +0100

    #10007 - Create an angular component for examination-create  #10044 - Create a service to return referral details  #10006 - As a user I want to be able to view a page that will allow me to create a new examination

--- Mep.Ui/src/app/components/examination/examination-create/examination-create.component.ts
+++ Mep.Ui/src/app/components/examination/examination-create/examination-create.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { ReferralService } from '../../../services/referral/referral.service';
-import { Referral } from 'src/app/interfaces/referral';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { LeadAmhpUser } from 'src/app/interfaces/user';
 import { Patient } from 'src/app/interfaces/patient';
+import { Referral } from 'src/app/interfaces/referral';
+import { ReferralService } from '../../../services/referral/referral.service';
+import { ToastService } from '../../../services/toast/toast.service';
 
 @Component({
   selector: 'app-examination-create',
@@ -10,33 +12,52 @@ import { Patient } from 'src/app/interfaces/patient';
   styleUrls: ['./examination-create.component.css']
 })
 export class ExaminationCreateComponent implements OnInit {
+  private referral = {} as Referral;
 
-  private referral: Referral;
+  isRetrievingReferralData: boolean;
+  dangerMessage: string;
+
+  @ViewChild('dangerToast', null) dangerTemplate;
 
   constructor(
-    private referralService: ReferralService
-  ) { }
+    private referralService: ReferralService,
+    private route: ActivatedRoute,
+    private toastService: ToastService
+  ) {}
 
   ngOnInit() {
-    this.referral = {} as Referral;
-    this.referral.id = null;
+
+    this.referral.id = parseInt(
+      this.route.snapshot.paramMap.get('referralId'),
+      null
+    );
     this.referral.leadAmhpUser = {} as LeadAmhpUser;
     this.referral.leadAmhpUser.displayName = null;
     this.referral.patient = {} as Patient;
     this.referral.patient.patientIdentifier = null;
 
-    console.log('get');
+    this.isRetrievingReferralData = false;
+
     // fetch the latest referral details
-    this.referralService
-    .getReferral(1009)
-    .subscribe(
+    this.referralService.getReferral(this.referral.id).subscribe(
       (referral: Referral) => {
-        console.log(referral);
+        this.isRetrievingReferralData = false;
         this.referral = referral;
-        // /this.referral.patient.patientIdentifier =
-        //this.referral.patient.nhsNumber != null ? this.referral.patient.nhsNumber.toString() : this.referral.patient.alternativeIdentifier;
-      });
+        this.referral.patient.patientIdentifier =
+          this.referral.patient.nhsNumber != null
+            ? this.referral.patient.nhsNumber.toString()
+            : this.referral.patient.alternativeIdentifier;
 
+            // ToDo: Inform the user if this referral already has an active examination
+      },
+      err => {
+        this.isRetrievingReferralData = false;
+        this.dangerMessage = `Unable to find referral with id: ${this.referral.id}`;
+        this.toastService.show(this.dangerTemplate, {
+          classname: 'bg-danger text-light',
+          delay: 10000
+        });
+      }
+    );
   }
-
 }
