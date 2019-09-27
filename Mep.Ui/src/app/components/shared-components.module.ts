164b476583de8594fb40647fc7c3aaaea2df4180
Mark Carter <mark.carter4@nhs.net>
2019-09-27 11:51:33 +0100

    Include template module

--- Mep.Ui/src/app/components/shared-components.module.ts
+++ Mep.Ui/src/app/components/shared-components.module.ts
@@ -1,21 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
 import { DisableControlDirective } from '../directives/disable-control/disable-control.directive';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TemplateModule } from '../templates/template.module';
 import { ToastsComponent } from './toasts/toasts.component';
 
 @NgModule({
   declarations: [
-    NavbarComponent,
     DisableControlDirective,
+    NavbarComponent,
+
     ToastsComponent
   ],
   imports: [
     CommonModule,
+    NgbModule,
     ReactiveFormsModule,
-    NgbModule
+    TemplateModule
   ],
   exports: [
     CommonModule,
@@ -23,6 +26,7 @@ import { ToastsComponent } from './toasts/toasts.component';
     NgbModule,
     NavbarComponent,
     DisableControlDirective,
+    TemplateModule,
     ToastsComponent
   ],
   providers: []
