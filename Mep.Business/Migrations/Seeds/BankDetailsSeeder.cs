925124a54ce2e12e0b0abaa0a8eb21d8305b6e8c
Stephen Harland <stephen.harland1@nhs.net>
2019-10-01 12:54:21 +0100

    created BankDetailsSeeder template  #10052 - Create seeder for BankDetails table

---
+++ Mep.Business/Migrations/Seeds/BankDetailsSeeder.cs
@@ -0,0 +1,19 @@
+using Mep.Data.Entities;
+using System.Linq;
+using System;
+
+namespace Mep.Business.Migrations.Seeds
+{
+  internal class BankDetailsSeeder : SeederBase
+  {
+
+    internal BankDetailsSeeder(ApplicationContext context)
+      : base(context)
+    {
+    }
+
+    internal void SeedData()
+    {
+    }
+  }
+}
