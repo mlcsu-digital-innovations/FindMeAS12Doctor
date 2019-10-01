10baabfa745bb8f68e147d1389e55d84c0da1b7b
Stephen Harland <stephen.harland1@nhs.net>
2019-10-01 13:31:27 +0100

    ContactDetailsSeeder template  #10054 - Create seeder for ContactDetails table

---
+++ Mep.Business/Migrations/Seeds/ContactDetailsSeeder.cs
@@ -0,0 +1,19 @@
+using Mep.Data.Entities;
+using System.Linq;
+using System;
+
+namespace Mep.Business.Migrations.Seeds
+{
+  internal class ContactDetailsSeeder : SeederBase
+  {
+
+    internal ContactDetailsSeeder(ApplicationContext context)
+      : base(context)
+    {
+    }
+
+    internal void SeedData()
+    {
+    }
+  }
+}
