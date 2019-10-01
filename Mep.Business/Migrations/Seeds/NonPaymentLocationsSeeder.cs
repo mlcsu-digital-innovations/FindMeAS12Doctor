b3e89ab8312e212853bc3c6d2a9a42393bac7040
Stephen Harland <stephen.harland1@nhs.net>
2019-10-01 14:28:51 +0100

    NonPaymentLocationsSeeder template  #10057 - Create seeder for NonPaymentLocations table

---
+++ Mep.Business/Migrations/Seeds/NonPaymentLocationsSeeder.cs
@@ -0,0 +1,19 @@
+using Mep.Data.Entities;
+using System.Linq;
+using System;
+
+namespace Mep.Business.Migrations.Seeds
+{
+  internal class NonPaymentLocationsSeeder : SeederBase
+  {
+
+    internal NonPaymentLocationsSeeder(ApplicationContext context)
+      : base(context)
+    {
+    }
+
+    internal void SeedData()
+    {
+    }
+  }
+}
