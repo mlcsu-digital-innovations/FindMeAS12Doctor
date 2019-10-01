8690e99a51fe71cc914d46764a1001838c45ae25
Stephen Harland <stephen.harland1@nhs.net>
2019-10-01 14:20:35 +0100

    DoctorStatusesSeeder template  #10056 - Create seeder for DoctorStatuses table

---
+++ Mep.Business/Migrations/Seeds/DoctorStatusesSeeder.cs
@@ -0,0 +1,19 @@
+using Mep.Data.Entities;
+using System.Linq;
+using System;
+
+namespace Mep.Business.Migrations.Seeds
+{
+  internal class DoctorStatusesSeeder : SeederBase
+  {
+
+    internal DoctorStatusesSeeder(ApplicationContext context)
+      : base(context)
+    {
+    }
+
+    internal void SeedData()
+    {
+    }
+  }
+}
