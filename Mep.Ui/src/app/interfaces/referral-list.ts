fedfd73503a009b667b0bd1edd623b5ef3c64712
Neil B Davies <neil.davies11@nhs.net>
2019-10-01 20:16:57 +0100

    adding referral-list interface, fixes AB#9937

---
+++ Mep.Ui/src/app/interfaces/referral-list.ts
@@ -0,0 +1,12 @@
+export interface ReferralList {
+  doctorsAllocated: number;
+  examinationLocationPostcode: string;
+  leadAmhp: string;
+  numberOfExaminationAttempts: number;
+  patientIdentifier: string;
+  referralId: number;
+  responsesReceived: number;
+  speciality: string;
+  status: string;
+  timescale: Date;
+}
