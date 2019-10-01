aadcf6d201c2c98673c083702a320499d0e15c62
Stephen Harland <stephen.harland1@nhs.net>
2019-10-01 14:54:26 +0100

    OnCallUsersSeeder template  #10059 - Create seeder for OnCallUsers table

---
+++ Mep.Business/Migrations/Seeds/OnCallUsersSeeder.cs
@@ -0,0 +1,19 @@
+using Mep.Data.Entities;
+using System.Linq;
+using System;
+
+namespace Mep.Business.Migrations.Seeds
+{
+  internal class OnCallUsersSeeder : SeederBase
+  {
+
+    internal OnCallUsersSeeder(ApplicationContext context)
+      : base(context)
+    {
+    }
+
+    internal void SeedData()
+    {
+    }
+  }
+}
