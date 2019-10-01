68a4e9ac96475dbd017a34b2e3d0c59ba96b921a
Stephen Harland <stephen.harland1@nhs.net>
2019-10-01 13:02:42 +0100

    created ClaimStatuses template  #10053 - Create seeder for ClaimStatuses table

---
+++ Mep.Business/Migrations/Seeds/ClaimStatusesSeeder.cs
@@ -0,0 +1,19 @@
+using Mep.Data.Entities;
+using System.Linq;
+using System;
+
+namespace Mep.Business.Migrations.Seeds
+{
+  internal class ClaimStatusesSeeder : SeederBase
+  {
+
+    internal ClaimStatusesSeeder(ApplicationContext context)
+      : base(context)
+    {
+    }
+
+    internal void SeedData()
+    {
+    }
+  }
+}
