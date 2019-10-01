c98b27588f7c59cd5e6f33fbaaf4d17aecca713f
Stephen Harland <stephen.harland1@nhs.net>
2019-10-01 15:13:56 +0100

    PaymentMethodTypesSeeder template  #10061 - Create seeder for PaymentMethodTypes table

---
+++ Mep.Business/Migrations/Seeds/PaymentMethodTypesSeeder.cs
@@ -0,0 +1,19 @@
+using Mep.Data.Entities;
+using System.Linq;
+using System;
+
+namespace Mep.Business.Migrations.Seeds
+{
+  internal class PaymentMethodTypesSeeder : SeederBase
+  {
+
+    internal PaymentMethodTypesSeeder(ApplicationContext context)
+      : base(context)
+    {
+    }
+
+    internal void SeedData()
+    {
+    }
+  }
+}
