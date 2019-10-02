d7b5be6f2b3fc10b9e524b00678edb105bbdec43
Stephen Harland <stephen.harland1@nhs.net>
2019-10-02 09:37:12 +0100

    UnsuccessfulExaminationTypesSeeder
    Created Template
     AB#10065 - Create seeder for UnsuccessfulExaminationTypes table

---
+++ Mep.Business/Migrations/Seeds/UnsuccessfulExaminationTypesSeeder.cs
@@ -0,0 +1,19 @@
+using Mep.Data.Entities;
+using System.Linq;
+using System;
+
+namespace Mep.Business.Migrations.Seeds
+{
+  internal class UnsuccessfulExaminationTypesSeeder : SeederBase
+  {
+
+    internal UnsuccessfulExaminationTypesSeeder(ApplicationContext context)
+      : base(context)
+    {
+    }
+
+    internal void SeedData()
+    {
+    }
+  }
+}
