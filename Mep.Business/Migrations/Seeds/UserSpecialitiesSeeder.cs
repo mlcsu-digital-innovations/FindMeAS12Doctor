e5c9f699e358dc4c9f30ab12967547bba602ec62
Stephen Harland <stephen.harland1@nhs.net>
2019-10-02 09:38:58 +0100

    Created UserSpecialitiesSeeder Template  AB#10066 - Create seeder for UserSpecialities table

---
+++ Mep.Business/Migrations/Seeds/UserSpecialitiesSeeder.cs
@@ -0,0 +1,19 @@
+using Mep.Data.Entities;
+using System.Linq;
+using System;
+
+namespace Mep.Business.Migrations.Seeds
+{
+  internal class UserSpecialitiesSeeder : SeederBase
+  {
+
+    internal UserSpecialitiesSeeder(ApplicationContext context)
+      : base(context)
+    {
+    }
+
+    internal void SeedData()
+    {
+    }
+  }
+}
