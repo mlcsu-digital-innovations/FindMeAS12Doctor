e822b3017211218f93030dbccda743b9848807c3
Stephen Harland <stephen.harland1@nhs.net>
2019-10-02 09:33:57 +0100

    created PaymentRuleSetsSeeder template  AB#10063 - Create seeder for PaymentRuleSets table

---
+++ Mep.Business/Migrations/Seeds/PaymentRuleSetsSeeder.cs
@@ -0,0 +1,19 @@
+using Mep.Data.Entities;
+using System.Linq;
+using System;
+
+namespace Mep.Business.Migrations.Seeds
+{
+  internal class PaymentRuleSetsSeeder : SeederBase
+  {
+
+    internal PaymentRuleSetsSeeder(ApplicationContext context)
+      : base(context)
+    {
+    }
+
+    internal void SeedData()
+    {
+    }
+  }
+}
