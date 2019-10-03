fe71012cf74f8151c606fa2f393de687d7fd9021
Mark Carter <mark.carter4@nhs.net>
2019-10-03 09:22:22 +0100

    fix unit test for FocusOnShow directive

--- Mep.Ui/src/app/directives/focus-on-show/focus-on-show.directive.spec.ts
+++ Mep.Ui/src/app/directives/focus-on-show/focus-on-show.directive.spec.ts
@@ -1,8 +1,38 @@
 import { FocusOnShowDirective } from './focus-on-show.directive';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestFocusOnShowComponent } from './TestFocusOnShowComponent';
+import { NgControl, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
-describe('FocusOnShowDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FocusOnShowDirective();
-    expect(directive).toBeTruthy();
+fdescribe('FocusOnShowDirective', () => {
+  let component: TestFocusOnShowComponent;
+  let fixture: ComponentFixture<TestFocusOnShowComponent>;
+  let InputElement: DebugElement;
+
+  const formBuilder: FormBuilder = new FormBuilder();
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestFocusOnShowComponent, FocusOnShowDirective],
+      imports: [ReactiveFormsModule],
+      providers: [NgControl, { provide: FormBuilder, useValue: formBuilder }]
+    }).compileComponents();
+  });
+
+  function createComponent() {
+    fixture = TestBed.createComponent(TestFocusOnShowComponent);
+    component = fixture.componentInstance;
+    InputElement = fixture.debugElement.nativeElement.querySelector('input');
+
+    component.testForm = formBuilder.group({
+      otherField: '',
+      focusField: ''
+    });
+    fixture.detectChanges();
+  }
+
+  it('should create an instance and focus on focusField', () => {
+    createComponent();
+    expect(fixture.debugElement.nativeElement.ownerDocument.activeElement.name).toEqual('focusField');
   });
 });
