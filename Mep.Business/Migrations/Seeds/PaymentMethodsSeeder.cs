738112235e7f6a51c51b7e74e185b8d2a8e30cab
Stephen Harland <stephen.harland1@nhs.net>
2019-10-03 12:56:50 +0100

    PaymentMethodsSeeder test data
    Added test data for male and female users
     AB#10060 - Create seeder for PaymentMethods table

--- Mep.Business/Migrations/Seeds/PaymentMethodsSeeder.cs
+++ Mep.Business/Migrations/Seeds/PaymentMethodsSeeder.cs
@@ -14,6 +14,35 @@ namespace Mep.Business.Migrations.Seeds
 
     internal void SeedData()
     {
+      PaymentMethod paymentMethod;
+
+      if ((paymentMethod = _context.PaymentMethods
+        .SingleOrDefault(g => g.UserId == GetUserIdByDisplayname(USER_DISPLAY_NAME_DOCTOR_FEMALE))) == null)
+      {
+        paymentMethod = new PaymentMethod();
+        _context.Add(paymentMethod);
+      }
+      paymentMethod.CcgId = GetFirstCcg();
+      paymentMethod.IsActive = true;
+      paymentMethod.ModifiedAt = _now;
+      paymentMethod.ModifiedByUser = GetSystemAdminUser();
+      // TODO: replace PaymentMethodTypeId = 1 with Get function when PaymentMethodTypeSeeder is populated with data
+      paymentMethod.PaymentMethodTypeId = 1;
+      paymentMethod.UserId = GetUserIdByDisplayname(USER_DISPLAY_NAME_DOCTOR_FEMALE);
+
+      if ((paymentMethod = _context.PaymentMethods
+        .SingleOrDefault(g => g.UserId == GetUserIdByDisplayname(USER_DISPLAY_NAME_DOCTOR_MALE))) == null)
+      {
+        paymentMethod = new PaymentMethod();
+        _context.Add(paymentMethod);
+      }
+      paymentMethod.CcgId = GetFirstCcg();
+      paymentMethod.IsActive = true;
+      paymentMethod.ModifiedAt = _now;
+      paymentMethod.ModifiedByUser = GetSystemAdminUser();
+      // TODO: replace PaymentMethodTypeId = 1 with Get function when PaymentMethodTypeSeeder is populated with data
+      paymentMethod.PaymentMethodTypeId = 1;
+      paymentMethod.UserId = GetUserIdByDisplayname(USER_DISPLAY_NAME_DOCTOR_MALE);
     }
   }
 }
