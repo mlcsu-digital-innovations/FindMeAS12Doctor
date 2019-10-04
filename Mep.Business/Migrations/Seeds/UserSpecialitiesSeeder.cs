b8ea2820e6455da15fe635e702631d1976bc9ccb
Stephen Harland <stephen.harland1@nhs.net>
2019-10-04 12:02:39 +0100

    Added temp data to UserSpecialitiesSeeder
     AB#10066 - Create seeder for UserSpecialities table

--- Mep.Business/Migrations/Seeds/UserSpecialitiesSeeder.cs
+++ Mep.Business/Migrations/Seeds/UserSpecialitiesSeeder.cs
@@ -14,6 +14,35 @@ namespace Mep.Business.Migrations.Seeds
 
     internal void SeedData()
     {
+      UserSpeciality userSpeciality;
+
+      if ((userSpeciality = _context
+        .UserSpecialities
+          .SingleOrDefault(g => g.UserId == GetUserIdByDisplayname(USER_DISPLAY_NAME_DOCTOR_MALE)))
+            == null)
+      {
+        userSpeciality = new UserSpeciality();
+        _context.Add(userSpeciality);
+      }
+      userSpeciality.IsActive = true;
+      userSpeciality.ModifiedAt = _now;
+      userSpeciality.ModifiedByUser = GetSystemAdminUser();
+      userSpeciality.SpecialityId = GetSpecialityId();
+      userSpeciality.UserId = GetUserIdByDisplayname(USER_DISPLAY_NAME_DOCTOR_MALE);
+
+      if ((userSpeciality = _context
+        .UserSpecialities
+          .SingleOrDefault(g => g.UserId == GetUserIdByDisplayname(USER_DISPLAY_NAME_DOCTOR_FEMALE)))
+            == null)
+      {
+        userSpeciality = new UserSpeciality();
+        _context.Add(userSpeciality);
+      }
+      userSpeciality.IsActive = true;
+      userSpeciality.ModifiedAt = _now;
+      userSpeciality.ModifiedByUser = GetSystemAdminUser();
+      userSpeciality.SpecialityId = GetSpecialityId();
+      userSpeciality.UserId = GetUserIdByDisplayname(USER_DISPLAY_NAME_DOCTOR_FEMALE);
     }
   }
 }
