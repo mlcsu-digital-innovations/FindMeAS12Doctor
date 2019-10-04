92bdd0c4033a73eeaa74ba0a7dbd3ae26cc64bcd
Mark Carter <mark.carter4@nhs.net>
2019-10-04 10:52:45 +0100

    fix new toast template Now uses single component for all toast messages

---
+++ Mep.Ui/src/app/interfaces/toast-options.ts
@@ -0,0 +1,8 @@
+export interface ToastOptions {
+  classname?: string;
+  delay?: number;
+  hide?: boolean;
+  iconClass?: string;
+  message: string;
+  title?: string;
+}
--- Mep.Ui/src/app/services/toast/toast.service.ts
+++ Mep.Ui/src/app/services/toast/toast.service.ts
@@ -1,11 +1,41 @@
 import { Injectable, TemplateRef } from '@angular/core';
+import { ToastOptions } from 'src/app/interfaces/toast-options';
 
 @Injectable({ providedIn: 'root' })
 export class ToastService {
   toasts: any[] = [];
 
   show(textOrTpl: string | TemplateRef<any>, options: any = {}) {
+
+    options.classname += ' text-light fade-in toast-container';
+
     this.toasts.push({ textOrTpl, ...options });
+  }
+
+  displayError(textOrTpl: string | TemplateRef<any>, options: ToastOptions) {
+    options.delay = 60000;
+    options.classname = 'bg-danger';
+    options.hide = false;
+    options.iconClass = 'fas fa-times-circle';
+    this.show(textOrTpl, options);
+  }
+
+  displayWarning(textOrTpl: string | TemplateRef<any>, options: ToastOptions) {
+    options.classname = 'bg-warning';
+    options.iconClass = 'fas fa-exclamation-triangle';
+    this.show(textOrTpl, options);
+  }
+
+  displayInfo(textOrTpl: string | TemplateRef<any>, options: ToastOptions) {
+    options.classname = 'bg-info';
+    options.iconClass = 'fas fa-info-circle';
+    this.show(textOrTpl, options);
+  }
+
+  displaySuccess(textOrTpl: string | TemplateRef<any>, options: ToastOptions) {
+    options.classname = 'bg-success';
+    options.iconClass = 'fas fa-check';
+    this.show(textOrTpl, options);
   }
 
   remove(toast) {
