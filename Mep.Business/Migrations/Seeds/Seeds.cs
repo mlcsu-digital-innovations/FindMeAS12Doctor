dd84558b7eebb0e7101c47d586f6d10b481e39ce
Stephen Harland <stephen.harland1@nhs.net>
2019-10-08 15:13:29 +0100

    Removed some code duplication
    Replaced duplicate seeds with running SeedAllNoGp from within SeedAll
    
     AB#10075 - Update formatting of long lines of code

--- Mep.Business/Migrations/Seeds/Seeds.cs
+++ Mep.Business/Migrations/Seeds/Seeds.cs
@@ -12,37 +12,12 @@ namespace Mep.Business.Migrations.Seeds
       new SystemAdminUserSeeder(_context).SeedData();
       _context.SaveChanges();
 
-      // create all CCGs
-      new CcgSeeder(_context).SeedData();
-      _context.SaveChanges();
-
       // create all GP Practices
       new GpPracticeSeeder(_context).SeedData();
       _context.SaveChanges();
 
-      new ClaimStatusesSeeder(_context).SeedData();
-
-      new ContactDetailTypesSeeder(_context).SeedData();
-
-      new DoctorStatusesSeeder(_context).SeedData();
-
-      new GenderTypeSeeder(_context).SeedData();
-
-      new NonPaymentLocationTypesSeeder(_context).SeedData();
-
-      new PaymentMethodTypesSeeder(_context).SeedData();
-
-      new PaymentRuleSetsSeeder(_context).SeedData();
-
-      new ProfileTypeSeeder(_context).SeedData();
-
-      new ReferralStatusSeeder(_context).SeedData();
-
-      new Section12ApprovalStatusesSeeder(_context).SeedData();
-
-      new SpecialitySeeder(_context).SeedData();
-
-      new UnsuccessfulExaminationTypesSeeder(_context).SeedData();
+      // run all other seeders from SeedAllNoGp
+      SeedAllNoGp();
 
       _context.SaveChanges();
     }
