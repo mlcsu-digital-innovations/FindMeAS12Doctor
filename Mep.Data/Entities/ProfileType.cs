7016baf35fef2f784a1ee13f0fa9b5f151031dc6
Neil B Davies <neil.davies11@nhs.net>
2019-10-09 10:01:26 +0100

    Fix EF 3.0 issue with entity property
    Refactoring enum to entity constants
    Removing NotMapped IsAmhp property

--- Mep.Data/Entities/ProfileType.cs
+++ Mep.Data/Entities/ProfileType.cs
@@ -5,14 +5,13 @@ namespace Mep.Data.Entities
 {
   public class ProfileType : NameDescription, IProfileType
   {
+    public const int SYSTEM = 1;
+    public const int AMHP = 2;
+    public const int DOCTOR = 3;
+    public const int FINANCE = 4;
+
     [InverseProperty("ProfileType")]
     public virtual IList<User> Users { get; set; }
-
-    [NotMapped]
-    public bool IsAmhp {
-      get{
-        return string.Compare(this.Name, "AMHP", System.StringComparison.InvariantCultureIgnoreCase) == 0;
-      }
-    }
+    
   }
 }
