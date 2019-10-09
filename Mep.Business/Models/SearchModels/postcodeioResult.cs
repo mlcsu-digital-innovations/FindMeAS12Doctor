620a0dd8e30e09a1f5528370d1f633e34f11796e
Neil B Davies <neil.davies11@nhs.net>
2019-10-09 10:04:45 +0100

    Refactor formatting and alphabetical ordering

--- Mep.Business/Models/SearchModels/postcodeioResult.cs
+++ Mep.Business/Models/SearchModels/postcodeioResult.cs
@@ -7,40 +7,40 @@ namespace Mep.Business.Models.SearchModels
     }
 
     public class Result {
-        public string Postcode {get; set;}
-        public int Quality {get; set;}
-        public int Eastings {get; set;}
-        public int Northings {get; set;}
+
+        public string Admin_County {get; set;}
+        public string Admin_District {get; set;}
+        public string Admin_Ward {get; set;}
+        public string Ccg {get; set;}
+        public string Ced {get; set;}
+        public Code Codes {get; set;}
         public string Country {get; set;}
-        public string Nhs_ha {get; set;}
-        public decimal Longitude {get; set;}
+        public int Eastings {get; set;}
+        public string Incode {get; set;}
+        public string European_Electoral_Region {get; set;}
         public decimal Latitude {get; set;}
-        public string European_electoral_region {get; set;}
-        public string Primary_care_trust {get; set;}
-        public string Region {get; set;}
+        public decimal Longitude {get; set;}        
         public string Lsoa {get; set;}
         public string Msoa {get; set;}
-        public string  Incode {get; set;}
+        public string Nhs_ha {get; set;}
+        public int Northings {get; set;}
+        public string Nuts {get; set;}        
         public string Outcode {get; set;}
-        public string admin_district {get; set;}
-        public string admin_county {get; set;}
-        public string admin_ward {get; set;}
-        public string ced {get; set;}
         public string Parish {get; set;}
-        public string Ccg {get; set;}
-        public string Nuts {get; set;}
         public string Parliamentary_constituency {get; set;}
-
-        public Code Codes {get; set;}
+        public string Postcode {get; set;}
+        public string Primary_care_trust {get; set;}
+        public int Quality {get; set;}
+        public string Region {get; set;}
 
     }
 
     public class Code {
-      public string Admin_district {get; set;}
-      public string Admin_county {get; set;}
-      public string Admin_ward {get; set;}
+      public string Admin_District {get; set;}
+      public string Admin_County {get; set;}
+      public string Admin_Ward {get; set;}
       public string Parish {get; set;}
-      public string Parliamentary_constituency {get; set;}
+      public string Parliamentary_Constituency {get; set;}
       public string Ccg {get; set;}
       public string Ced {get; set;}
       public string Nuts {get; set;}
