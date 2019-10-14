604b68102106013a7f44ac2f0f188ed757f5dbf4
Mark Carter <mark.carter4@nhs.net>
2019-10-14 09:37:06 +0100

    fix unit tests

--- Mep.Ui/src/app/services/examination/examination.service.spec.ts
+++ Mep.Ui/src/app/services/examination/examination.service.spec.ts
@@ -1,9 +1,12 @@
+import { ExaminationService } from './examination.service';
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
-import { ExaminationService } from './examination.service';
-
 describe('ExaminationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [],
+    imports: [HttpClientModule]
+  }));
 
   it('should be created', () => {
     const service: ExaminationService = TestBed.get(ExaminationService);
