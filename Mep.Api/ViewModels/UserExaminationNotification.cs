8ec9b2d00be5267cedf2a63441b5bad046ca4a57
Neil B Davies <neil.davies11@nhs.net>
2019-10-17 11:28:23 +0100

    Add examination save
    Database update required
    Invalid properties create a 400 rather than a 500 status code
    Adds validation to examination creation properties
    Moving constants from Entities to Business Models

--- Mep.Api/ViewModels/UserExaminationNotification.cs
+++ Mep.Api/ViewModels/UserExaminationNotification.cs
@@ -1,7 +1,13 @@
+using System;
+
 namespace Mep.Api.ViewModels
 {
-    public class UserExaminationNotification
-    {
-        
-    }
+  public class UserExaminationNotification
+  {
+    public int ExaminationId { get; set; }
+    public bool? HasAccepted { get; set; }
+    public int NotificationTextId { get; set; }
+    public DateTimeOffset? RespondedAt { get; set; }
+    public int UserId { get; set; }
+  }
 }
--- Mep.Business/Migrations/Seeds/NotificationTextsSeeder.cs
+++ Mep.Business/Migrations/Seeds/NotificationTextsSeeder.cs
@@ -12,19 +12,19 @@ namespace Mep.Business.Migrations.Seeds
       if ((notificationText = _context
         .NotificationTexts
           .SingleOrDefault(g => g.Name ==
-            NOTIFICATION_TEXT_NAME_1)) == null)
+            NOTIFICATION_TEXT_NAME_ASSIGNED_TO_EXAMINATION)) == null)
       {
         notificationText = new NotificationText();
         _context.Add(notificationText);
       }
       notificationText.Description =
-        NOTIFICATION_TEXT_DESCRIPTION_1;
+        NOTIFICATION_TEXT_DESCRIPTION_ASSIGNED_TO_EXAMINATION;
       notificationText.IsActive = true;
       notificationText.MessageTemplate =
-        NOTIFICATION_TEXT_MESSAGE_TEMPLATE_1;
+        NOTIFICATION_TEXT_MESSAGE_TEMPLATE_ASSIGNED_TO_EXAMINATION;
       notificationText.ModifiedAt = _now;
       notificationText.ModifiedByUser = GetSystemAdminUser();
-      notificationText.Name = NOTIFICATION_TEXT_NAME_1;
+      notificationText.Name = NOTIFICATION_TEXT_NAME_ASSIGNED_TO_EXAMINATION;
 
       if ((notificationText = _context
         .NotificationTexts
