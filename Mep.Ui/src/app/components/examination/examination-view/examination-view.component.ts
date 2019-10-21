cb8c6f63f6ca45f3b7380b7b4a1647c434d3b4b2
Stephen Harland <stephen.harland1@nhs.net>
2019-10-21 11:47:33 +0100

    Created view examination page header
    Populated header with data
    
     AB#10171 - Create view examinations page header

--- Mep.Ui/src/app/components/examination/examination-view/examination-view.component.ts
+++ Mep.Ui/src/app/components/examination/examination-view/examination-view.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NhsNumberValidFormat } from 'src/app/helpers/nhs-number.validator';
+import { Observable, of } from 'rxjs';
+import { ParamMap, ActivatedRoute, Router } from '@angular/router';
+import { Patient } from 'src/app/interfaces/patient';
+import { Referral } from 'src/app/interfaces/referral';
+import { ReferralService } from 'src/app/services/referral/referral.service';
+import { switchMap, map, catchError } from 'rxjs/operators';
+import { ToastService } from 'src/app/services/toast/toast.service';
 
 @Component({
   selector: 'app-examination-view',
@@ -7,9 +16,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExaminationViewComponent implements OnInit {
 
-  constructor() { }
+  isPatientIdValidated: boolean;
+  referral$: Observable<Referral | any>;
+  referralCreated: Date;
+  referralForm: FormGroup;
+  referralId: number;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private referralService: ReferralService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastService: ToastService
+  ) { }
 
   ngOnInit() {
+    this.referral$ = this.route.paramMap.pipe(
+      switchMap(
+        (params: ParamMap) => {
+          return this.referralService.getReferral(+params.get('referralId'))
+            .pipe(
+              map(referral => {
+                this.InitialiseForm(referral);
+                return referral;
+              })
+            );
+        }
+      ),
+      catchError((err) => {
+        console.log(err);
+        this.toastService.displayError({
+          title: 'Error',
+          message: 'Error Retrieving Referral Information'
+        });
+
+        const emptyReferral = {} as Referral;
+
+        return of(emptyReferral);
+      })
+    );
+
+  }
+
+
+  InitialiseForm(referral: Referral) {
+    this.referralCreated = referral.createdAt;
+    this.referralId = referral.id;
+
+    console.log(referral);
   }
 
 }
