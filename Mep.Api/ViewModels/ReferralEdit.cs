9f378d7f00907334ef5a6895a275ba6ab6f20eef
Mark Carter <mark.carter4@nhs.net>
2019-10-22 10:47:19 +0100

    fix ordering of properties

--- Mep.Api/ViewModels/ReferralEdit.cs
+++ Mep.Api/ViewModels/ReferralEdit.cs
@@ -6,14 +6,14 @@ namespace Mep.Api.ViewModels
   {
     public DateTimeOffset CreatedAt { get; set; }
     public int Id { get; set; }
-    public int LeadAmhpUserId { get; set; }
-    public int? PatientCcgId { get; set; }
-    public int? PatientGpPracticeId { get; set; }
-    public long? PatientNhsNumber { get; set; }
     public string LeadAmhpUserDisplayName {get; set;}
+    public int LeadAmhpUserId { get; set; }    
     public string PatientAlternativeIdentifier { get; set; }
+    public int? PatientCcgId { get; set; }
     public string PatientCcgName {get; set;}
     public string PatientGpNameAndPostcode {get; set;}
+    public int? PatientGpPracticeId { get; set; }
+    public long? PatientNhsNumber { get; set; }
     public string PatientResidentialPostcode { get; set; }
     public string Status { get; set; }
   }
