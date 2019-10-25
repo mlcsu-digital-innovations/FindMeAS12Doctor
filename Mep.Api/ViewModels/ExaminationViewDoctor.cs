d46f3610ddc9b9cc2ad06321618aa3f2f7036b26
Neil B Davies <neil.davies11@nhs.net>
2019-10-25 11:21:27 +0100

    Add GMC no. to examination view doctor view model
    Adding gmc numbers for all doctor seeds

--- Mep.Api/ViewModels/ExaminationViewDoctor.cs
+++ Mep.Api/ViewModels/ExaminationViewDoctor.cs
@@ -3,6 +3,7 @@ namespace Mep.Api.ViewModels
   public class ExaminationViewDoctor
   {
     public string DisplayName { get; set; }
+    public int GmcNumber { get; set; }
     public int Id { get; set; }
   }
 }
