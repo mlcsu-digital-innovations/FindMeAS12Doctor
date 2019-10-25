1785b0417b60f36b95ef7e215c227529e676d4ba
Mark Carter <mark.carter4@nhs.net>
2019-10-25 15:52:11 +0100

    fix typeahead for doctor selection

--- Mep.Ui/src/app/components/doctor/doctor-select/doctor-select.component.ts
+++ Mep.Ui/src/app/components/doctor/doctor-select/doctor-select.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
-import { GpPracticeListService } from '../../../services/gp-practice-list/gp-practice-list.service';
-import { tap, switchMap, catchError } from 'rxjs/operators';
+import { DoctorListService } from 'src/app/services/doctor-list/doctor-list.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Observable, of } from 'rxjs';
+import { tap, switchMap, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-doctor-select',
@@ -11,21 +12,32 @@ import { Observable, of } from 'rxjs';
 })
 export class DoctorSelectComponent implements OnInit {
 
+  doctorForm: FormGroup;
   hasDoctorSearchFailed: boolean;
   isDoctorFieldsShown: boolean;
   isDoctorSearching: boolean;
   unknownDoctorId: number;
 
   constructor(
-    private doctorListService: GpPracticeListService,
+    private doctorListService: DoctorListService,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
     this.unknownDoctorId = 0;
+
+    this.doctorForm = this.formBuilder.group({
+      searchDoctor: []
+    });
+
   }
 
   FormatTypeAheadResults(value: any): string {
     return value.resultText || '';
+  }
+
+  get doctorField() {
+    return this.doctorForm.controls.searchDoctor;
   }
 
   DoctorSearch = (text$: Observable<string>) =>
@@ -34,7 +46,7 @@ export class DoctorSelectComponent implements OnInit {
       distinctUntilChanged(),
       tap(() => (this.isDoctorSearching = true)),
       switchMap(term =>
-        this.doctorListService.GetGpPracticeList(term).pipe(
+        this.doctorListService.GetDoctorList(term).pipe(
           tap(() => (this.hasDoctorSearchFailed = false)),
           catchError(() => {
             this.hasDoctorSearchFailed = true;
