05610a8f70f2b9002c5cb856bcd60596a3756577
Mark Carter <mark.carter4@nhs.net>
2019-10-25 11:04:42 +0100

    update oninit

--- Mep.Ui/src/app/components/doctor/doctor-select/doctor-select.component.ts
+++ Mep.Ui/src/app/components/doctor/doctor-select/doctor-select.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-doctor-select',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorSelectComponent implements OnInit {
 
-  constructor() { }
+  selectDoctor: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+
+    this.selectDoctor = this.formBuilder.group({
+     doctorSearch: []
+    });
+
   }
 
 }
