721219d2c2fd4c050f3c7bbf68ab11ea91e2fdbe
Stephen Harland <stephen.harland1@nhs.net>
2019-10-25 09:14:18 +0100

    Converted Search Doctor into typeahead field
    will need to be doctor service when available
    
     AB#10228 - Convert Search Doctor into typeahead field

--- Mep.Ui/src/app/components/doctor/doctor-select/doctor-select.component.ts
+++ Mep.Ui/src/app/components/doctor/doctor-select/doctor-select.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { GpPracticeListService } from '../../../services/gp-practice-list/gp-practice-list.service';
+import { tap, switchMap, catchError } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-doctor-select',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorSelectComponent implements OnInit {
 
-  constructor() { }
+  hasDoctorSearchFailed: boolean;
+  isDoctorFieldsShown: boolean;
+  isDoctorSearching: boolean;
+  unknownDoctorId: number;
+
+  constructor(
+    private doctorListService: GpPracticeListService,
+  ) { }
 
   ngOnInit() {
+    this.unknownDoctorId = 0;
   }
 
+  FormatTypeAheadResults(value: any): string {
+    return value.resultText || '';
+  }
+
+  DoctorSearch = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      tap(() => (this.isDoctorSearching = true)),
+      switchMap(term =>
+        this.doctorListService.GetGpPracticeList(term).pipe(
+          tap(() => (this.hasDoctorSearchFailed = false)),
+          catchError(() => {
+            this.hasDoctorSearchFailed = true;
+            return of([]);
+          })
+        )
+      ),
+      tap(() => (this.isDoctorSearching = false))
+    )
 }
