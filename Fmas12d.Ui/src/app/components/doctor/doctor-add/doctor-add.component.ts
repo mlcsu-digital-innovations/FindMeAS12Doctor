d9bd8c53e095c37f5e78f33afdaefd4ee5e803cd
Mark Carter <mark.carter4@nhs.net>
2019-11-08 15:56:34 +0000

    add field for gmc number

--- Fmas12d.Ui/src/app/components/doctor/doctor-add/doctor-add.component.ts
+++ Fmas12d.Ui/src/app/components/doctor/doctor-add/doctor-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-doctor-add',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorAddComponent implements OnInit {
 
-  constructor() { }
+  registeredDoctorForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.registeredDoctorForm = this.formBuilder.group({
+      gmcNumber: ['',
+      [
+        Validators.pattern('^[1-9]\\d{9}$')
+      ]]
+    });
   }
 
 }
