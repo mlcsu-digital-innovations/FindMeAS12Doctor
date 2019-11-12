eb20368187eb2f4b6c5271f482d7baa94cac3445
Mark Carter <mark.carter4@nhs.net>
2019-11-12 09:45:36 +0000

    reformat

--- Fmas12d.Ui/src/app/components/doctor/doctor.module.ts
+++ Fmas12d.Ui/src/app/components/doctor/doctor.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule, DecimalPipe } from '@angular/common';
+import { DoctorAddComponent } from './doctor-add/doctor-add.component';
 import { DoctorAllocateComponent } from './doctor-allocate/doctor-allocate.component';
 import { DoctorRoutes } from './doctor.routes';
 import { DoctorSelectComponent } from './doctor-select/doctor-select.component';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedComponentsModule } from '../shared-components.module';
-import { DoctorAddComponent } from './doctor-add/doctor-add.component';
 
 @NgModule({
   declarations: [
