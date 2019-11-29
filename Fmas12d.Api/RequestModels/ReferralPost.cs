0bb72870256d1b3de90559debb0a0d90229bb86a
Mark Carter <mark.carter4@nhs.net>
2019-11-29 09:19:07 +0000

    correct model mapping

--- Fmas12d.Api/RequestModels/ReferralPost.cs
+++ Fmas12d.Api/RequestModels/ReferralPost.cs
@@ -2,14 +2,15 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Fmas12d.Api.RequestModels
 {
-  public class ReferralPost : ReferralPut
+  public class ReferralPost 
   {
     internal virtual void MapToBusinessModel(Business.Models.ReferralCreate model)
     {
       model.LeadAmhpUserId = LeadAmhpUserId.Value;
       model.PatientId = PatientId.Value;
     }       
-
+    [Required]
+    public int? LeadAmhpUserId { get; set; }
     [Required]
     public int? PatientId { get; set; }
   }
--- Fmas12d.Api/RequestModels/ReferralRetrospectivePost.cs
+++ Fmas12d.Api/RequestModels/ReferralRetrospectivePost.cs
@@ -5,7 +5,7 @@ namespace Fmas12d.Api.RequestModels
 {
   public class ReferralRetrospectivePost : ReferralPost
   {
-    internal override void MapToBusinessModel(Business.Models.ReferralUpdate model)
+    internal override void MapToBusinessModel(Business.Models.ReferralCreate model)
     {
       base.MapToBusinessModel(model);
       model.CreatedAt = CreatedAt.Value;      
