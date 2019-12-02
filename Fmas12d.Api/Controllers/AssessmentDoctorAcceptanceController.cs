2849e4786b2e5259ed6d19be38ad70fc245338a0
Neil B Davies <neil.davies11@nhs.net>
2019-12-02 12:09:35 +0000

    Refactor assessment acceptance endpoints
    Non-admins need to use:
    /api/assessment/{assessmentid}/accepted
    in place of
    /api/assessment/{assessmentid}/doctors/{userid}/accepted
    etc.
    explicitly setting the userid is now only available to admins

--- Fmas12d.Api/Controllers/AssessmentDoctorAcceptanceController.cs
+++ Fmas12d.Api/Controllers/AssessmentDoctorAcceptanceController.cs
@@ -20,10 +20,27 @@ namespace Fmas12d.Api.Controllers
     }
 
     [HttpPut]
+    [Route("{assessmentId:int}/accepted")]
+    public async Task<ActionResult> PutAccepted(
+      int assessmentId
+    )
+    {
+      RequestModels.AssessmentDoctorAcceptance requestModel = 
+        new RequestModels.AssessmentDoctorAcceptance() {
+          AssessmentId = assessmentId,
+          HasAccepted = true,
+          UserId = GetUserId()
+      };
+      return await UpdateAcceptance(requestModel);
+    }   
+
+    [HttpPut]
+    [Authorize(Policy="Admin")]
     [Route("{assessmentId:int}/doctors/{userId:int}/accepted")]
     public async Task<ActionResult> PutAccepted(
       int assessmentId,
-      int userId)
+      int userId
+    )
     {
       RequestModels.AssessmentDoctorAcceptance requestModel = 
         new RequestModels.AssessmentDoctorAcceptance() {
@@ -32,14 +49,29 @@ namespace Fmas12d.Api.Controllers
           UserId = userId
       };
       return await UpdateAcceptance(requestModel);
-    }      
+    }    
 
     [HttpPut]
+    [Route("{assessmentId:int}/accepted/contactdetail")]
+    public async Task<ActionResult> PutAcceptedContactDetail(
+      int assessmentId,
+      [FromBody] RequestModels.AssessmentDoctorAcceptancePutContactDetail requestModel
+    )
+    {
+      requestModel.AssessmentId = assessmentId;
+      requestModel.HasAccepted = true;
+      requestModel.UserId = GetUserId();
+      return await UpdateAcceptance(requestModel);
+    }        
+
+    [HttpPut]
+    [Authorize(Policy="Admin")]
     [Route("{assessmentId:int}/doctors/{userId:int}/accepted/contactdetail")]
     public async Task<ActionResult> PutAcceptedContactDetail(
       int assessmentId,
       int userId,
-      [FromBody] RequestModels.AssessmentDoctorAcceptancePutContactDetail requestModel)
+      [FromBody] RequestModels.AssessmentDoctorAcceptancePutContactDetail requestModel
+    )
     {
       requestModel.AssessmentId = assessmentId;
       requestModel.HasAccepted = true;
@@ -48,11 +80,26 @@ namespace Fmas12d.Api.Controllers
     }
 
     [HttpPut]
+    [Route("{assessmentId:int}/accepted/location")]
+    public async Task<ActionResult> PutAcceptedLocation(
+      int assessmentId,
+      [FromBody] RequestModels.AssessmentDoctorAcceptancePutLocation requestModel
+    )
+    {
+      requestModel.AssessmentId = assessmentId;
+      requestModel.HasAccepted = true;
+      requestModel.UserId = GetUserId();
+      return await UpdateAcceptance(requestModel);
+    }
+
+    [HttpPut]
+    [Authorize(Policy="Admin")]
     [Route("{assessmentId:int}/doctors/{userId:int}/accepted/location")]
     public async Task<ActionResult> PutAcceptedLocation(
       int assessmentId,
       int userId,
-      [FromBody] RequestModels.AssessmentDoctorAcceptancePutLocation requestModel)
+      [FromBody] RequestModels.AssessmentDoctorAcceptancePutLocation requestModel
+    )
     {
       requestModel.AssessmentId = assessmentId;
       requestModel.HasAccepted = true;
@@ -61,11 +108,26 @@ namespace Fmas12d.Api.Controllers
     }
 
     [HttpPut]
+    [Route("{assessmentId:int}/accepted/postcode")]
+    public async Task<ActionResult> PutAcceptedPostcode(
+      int assessmentId,
+      [FromBody] RequestModels.AssessmentDoctorAcceptancePutPostcode requestModel
+    )
+    {
+      requestModel.AssessmentId = assessmentId;
+      requestModel.HasAccepted = true;
+      requestModel.UserId = GetUserId();
+      return await UpdateAcceptance(requestModel);
+    }      
+
+    [HttpPut]
+    [Authorize(Policy="Admin")]
     [Route("{assessmentId:int}/doctors/{userId:int}/accepted/postcode")]
     public async Task<ActionResult> PutAcceptedPostcode(
       int assessmentId,
       int userId,
-      [FromBody] RequestModels.AssessmentDoctorAcceptancePutPostcode requestModel)
+      [FromBody] RequestModels.AssessmentDoctorAcceptancePutPostcode requestModel
+    )
     {
       requestModel.AssessmentId = assessmentId;
       requestModel.HasAccepted = true;
@@ -74,10 +136,27 @@ namespace Fmas12d.Api.Controllers
     }   
 
     [HttpPut]
+    [Route("{assessmentId:int}/declined")]
+    public async Task<ActionResult> PutDeclined(
+      int assessmentId
+    )
+    {
+      RequestModels.AssessmentDoctorAcceptance requestModel = 
+        new RequestModels.AssessmentDoctorAcceptance() {
+          AssessmentId = assessmentId,
+          HasAccepted = false,
+          UserId = GetUserId()
+      };
+      return await UpdateAcceptance(requestModel);
+    }  
+
+    [HttpPut]
+    [Authorize(Policy="Admin")]
     [Route("{assessmentId:int}/doctors/{userId:int}/declined")]
     public async Task<ActionResult> PutDeclined(
       int assessmentId,
-      int userId)
+      int userId
+    )
     {
       RequestModels.AssessmentDoctorAcceptance requestModel = 
         new RequestModels.AssessmentDoctorAcceptance() {
