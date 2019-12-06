0bd58179786eda91215867d2fcb001e72a033bf9
Mark Carter <mark.carter4@nhs.net>
2019-12-06 11:10:35 +0000

    add post method

--- Fmas12d.Mobile/src/app/services/api/api.service.ts
+++ Fmas12d.Mobile/src/app/services/api/api.service.ts
@@ -10,51 +10,63 @@ import { tap, map, catchError } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService {    
-  private contentType: string = "application/json";
+export class ApiService {
+  private contentType = 'application/json';
   private accessToken: string;
 
-  constructor(        
-    private http: HttpClient, 
+  constructor(
+    private http: HttpClient,
     private logService: LogService,
     private networkService: NetworkService,
-    private offlineManager: OfflineManagerService,    
+    private offlineManager: OfflineManagerService,
     private storageService: StorageService
-    ) 
-    { }
- 
+    ) { }
+
   public get(url: string, storageKey: string): Observable<any> {
-    if (this.networkService.getCurrentNetworkStatus() === ConnectionStatus.Offline) {      
+    if (this.networkService.getCurrentNetworkStatus() === ConnectionStatus.Offline) {
       return this.storageService.getApiRequestData(storageKey);
-    }    
-    else {       
-      return this.http.get(url)          
+    } else {
+      return this.http.get(url)
         .pipe(
-          catchError(error => {      
-            this.logService.logError(error);            
-            throw new Error(error);            
+          catchError(error => {
+            this.logService.logError(error);
+            throw new Error(error);
           }),
           map(result => result),
           tap(result => this.storageService.storeApiRequestData(storageKey, result)
-        )      
-      );       
-    }                             
+        )
+      );
+    }
   }
 
   public put(url: string, body: any): Observable<any> {
     if (this.networkService.getCurrentNetworkStatus() === ConnectionStatus.Offline) {
-      return from(this.offlineManager.storeRequest(url, "PUT", body));
-    }
-    else {      
+      return from(this.offlineManager.storeRequest(url, 'PUT', body));
+    } else {
       return this.http.put(url, body)
         .pipe(
           catchError(error => {
             this.logService.logError(error);
-            throw new Error(error)
+            throw new Error(error);
           }
-        )        
-      );               
-    }        
+        )
+      );
+    }
+  }
+
+  public post(url: string, body: any): Observable<any> {
+    if (this.networkService.getCurrentNetworkStatus() === ConnectionStatus.Offline) {
+      return from(this.offlineManager.storeRequest(url, 'POST', body));
+    } else {
+      return this.http.post(url, body)
+        .pipe(
+          catchError(error => {
+            this.logService.logError(error);
+            throw new Error(error);
+          }
+        )
+      );
+    }
   }
 
 }
