ef9c93df845097b45b18984d7307abdc678810fb
Jehan-David Beyers <jehan-david.beyers@nhs.net>
2019-12-11 14:33:30 +0000

    Adding isS12 to AMHP assessment view doctors Adding isRegistered to AMHP assessment view doctors

--- Fmas12d.Mobile/src/app/models/amhp-assessment-selected-doctor.model.ts
+++ Fmas12d.Mobile/src/app/models/amhp-assessment-selected-doctor.model.ts
@@ -1,11 +1,13 @@
 import { KnownLocation } from './known-location.model';
 
-export class AmhpAssessmentSelectedDoctor {
-  id: number;
+export class AmhpAssessmentSelectedDoctor {  
   displayName: string;
   distance: number;
   doctorId: number;
   gmcNumber: number;
   hasResponded?: boolean;
+  id: number;
+  isRegistered?: boolean;
+  isS12?: boolean;
   knownLocation: KnownLocation;
 }
