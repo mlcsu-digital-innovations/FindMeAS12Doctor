ab99898b56ac0e09ee00232740379f1833843730
Mark Carter <mark.carter4@nhs.net>
2019-12-11 11:29:37 +0000

    refactor service

--- Fmas12d.Mobile/src/app/services/user-availability/user-availability.service.ts
+++ Fmas12d.Mobile/src/app/services/user-availability/user-availability.service.ts
@@ -1,6 +1,7 @@
 import { ApiService } from '../api/api.service';
 import { environment } from 'src/environments/environment';
 import { Injectable } from '@angular/core';
+import { UNAVAILABLE } from 'src/app/constants/app.constants';
 import { UserAvailability } from 'src/app/interfaces/user-availability.interface';
 import { UserAvailabilityPost } from 'src/app/interfaces/availability-post.interface';
 import { UserAvailabilityPut } from 'src/app/interfaces/availability-put.interface';
@@ -38,7 +39,7 @@ export class UserAvailabilityService {
       postType = 'postcode';
     }
 
-    if (userAvailability.isAvailable === false) {
+    if (userAvailability.statusId === UNAVAILABLE) {
       postType = 'unavailable';
     }
 
@@ -48,42 +49,42 @@ export class UserAvailabilityService {
 
   public postUserAvailability(userAvailability: UserAvailability) {
 
-    const postAvailability = {} as UserAvailabilityPost;
+    const availability = {} as UserAvailabilityPost;
 
-    postAvailability.start = userAvailability.start;
-    postAvailability.end = userAvailability.end;
-    postAvailability.isAvailable = userAvailability.isAvailable;
-    postAvailability.contactDetailId = userAvailability.location.contactDetailId;
-    postAvailability.postcode = userAvailability.location.postcode;
-    postAvailability.latitude = userAvailability.location.latitude;
-    postAvailability.longitude = userAvailability.location.longitude;
+    availability.start = userAvailability.start;
+    availability.end = userAvailability.end;
+    availability.contactDetailId = userAvailability.location.contactDetailId;
+    availability.postcode = userAvailability.location.postcode;
+    availability.latitude = userAvailability.location.latitude;
+    availability.longitude = userAvailability.location.longitude;
+    availability.statusId = userAvailability.statusId;
 
     const postType = this.getPostType(userAvailability);
 
     return (this.apiService.post(
       `${environment.apiEndpoint}/useravailabilities/${postType}`,
-      postAvailability
+      availability
     ));
   }
 
   public putUserAvailability(userAvailability: UserAvailability) {
 
-    const putAvailability = {} as UserAvailabilityPut;
+    const availability = {} as UserAvailabilityPut;
 
-    putAvailability.start = userAvailability.start;
-    putAvailability.end = userAvailability.end;
-    putAvailability.isAvailable = userAvailability.isAvailable;
-    putAvailability.contactDetailId = userAvailability.location.contactDetailId;
-    putAvailability.postcode = userAvailability.location.postcode;
-    putAvailability.latitude = userAvailability.location.latitude;
-    putAvailability.longitude = userAvailability.location.longitude;
-    putAvailability.id = userAvailability.id;
+    availability.start = userAvailability.start;
+    availability.end = userAvailability.end;
+    availability.contactDetailId = userAvailability.location.contactDetailId;
+    availability.postcode = userAvailability.location.postcode;
+    availability.latitude = userAvailability.location.latitude;
+    availability.longitude = userAvailability.location.longitude;
+    availability.id = userAvailability.id;
+    availability.statusId = userAvailability.statusId;
 
     const postType = this.getPostType(userAvailability);
 
     return (this.apiService.put(
-      `${environment.apiEndpoint}/useravailabilities/${putAvailability.id}/${postType}`,
-      putAvailability
+      `${environment.apiEndpoint}/useravailabilities/${availability.id}/${postType}`,
+      availability
     ));
   }
 
