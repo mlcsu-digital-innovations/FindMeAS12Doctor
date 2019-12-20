68877387fec549f0ccf3480ae044681529952d0d
Mark Carter <mark.carter4@nhs.net>
2019-12-20 15:31:21 +0000

    formatting changes

--- Fmas12d.Mobile/src/app/services/auth/auth.service.ts
+++ Fmas12d.Mobile/src/app/services/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { MsalService, BroadcastService } from '@azure/msal-angular';
 import { OAuthSettings } from 'src/oauth';
 import { Subscription, Observable, from } from 'rxjs';
@@ -7,33 +7,32 @@ import { StorageService } from '../storage/storage.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class AuthService implements OnDestroy{
   private subscription: Subscription;
 
   constructor(
-    private msalService: MsalService,    
+    private msalService: MsalService,
     private broadcastService: BroadcastService,
     private storageService: StorageService
-    ) 
-  {
-    this.subscription = this.broadcastService.subscribe("msal:acquireTokenFailure", (payload) => {
+    ) {
+    this.subscription = this.broadcastService.subscribe('msal:acquireTokenFailure', (payload) => {
       console.log(payload);
-    });    
+    });
   }
 
   public signIn(): void {
-    this.msalService.loginRedirect();  
+    this.msalService.loginRedirect();
   }
-  
+
   public signOut(): void {
     this.msalService.logout();
     this.storageService.clearAccessToken();
-  }     
+  }
 
   ngOnDestroy() {
     this.broadcastService.getMSALSubject().next(1);
-    if(this.subscription) {
-      this.subscription.unsubscribe();      
+    if (this.subscription) {
+      this.subscription.unsubscribe();
     }
   }
 }
