23de02eba40b1f2cb375fa28e313f0730ce9856f
Mark Carter <mark.carter4@nhs.net>
2020-01-08 09:50:09 +0000

    add details page for doctor assessment

--- Fmas12d.Mobile/src/app/pages/doctor-assessment-details/doctor-assessment-details.module.ts
+++ Fmas12d.Mobile/src/app/pages/doctor-assessment-details/doctor-assessment-details.module.ts
@@ -1,11 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
-
-import { IonicModule } from '@ionic/angular';
-
 import { DoctorAssessmentDetailsPage } from './doctor-assessment-details.page';
+import { CommonModule } from '@angular/common';
+import { ComponentsModule } from 'src/app/components/components.module';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
@@ -17,6 +16,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    ComponentsModule,
     FormsModule,
     IonicModule,
     RouterModule.forChild(routes)
--- Fmas12d.Mobile/src/app/pages/doctor-assessment-details/doctor-assessment-details.page.ts
+++ Fmas12d.Mobile/src/app/pages/doctor-assessment-details/doctor-assessment-details.page.ts
@@ -1,4 +1,9 @@
+import { ActivatedRoute } from '@angular/router';
+import { AmhpAssessmentService } from 'src/app/services/amhp-assessment/amhp-assessment.service';
+import { AmhpAssessmentView } from 'src/app/models/amhp-assessment-view.model';
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { ToastService } from 'src/app/services/toast/toast.service';
 
 @Component({
   selector: 'app-doctor-assessment-details',
@@ -7,9 +12,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorAssessmentDetailsPage implements OnInit {
 
-  constructor() { }
+  private loading: HTMLIonLoadingElement;
+  private assessment: AmhpAssessmentView;
+
+  constructor(
+    private route: ActivatedRoute,
+    private assessmentService: AmhpAssessmentService,
+    private loadingController: LoadingController,
+    private toastService: ToastService
+  ) { }
 
   ngOnInit() {
+    this.assessment = new AmhpAssessmentView();
+    this.assessment.detailTypes = [];
+    const assessmentId = +this.route.snapshot.paramMap.get('id');
+
+    if (assessmentId) {
+      const request = this.assessmentService.getView(assessmentId);
+      this.showLoading();
+
+      request.subscribe((result: AmhpAssessmentView) => {
+        console.log(result);
+        this.assessment = result;
+
+        this.closeLoading();
+      }, error => {
+        this.closeLoading();
+        this.toastService.displayError({
+          message: "Unable to retrieve assessment details"
+        });
+      });
+    }
+  }
+
+  closeLoading() {
+    if (this.loading) {
+      this.loading.dismiss();
+    }
+  }
+
+  async showLoading() {
+    this.loading = await this.loadingController.create({
+      message: 'Please wait',
+      spinner: 'lines',
+      duration: 5000
+    });
+    await this.loading.present();
   }
 
 }
