031999536a8875f7de753ce1adf9ca5dcf18b3b1
Neil B Davies <neil.davies11@nhs.net>
2020-01-17 17:07:38 +0000

    Add PUT method at assessment/{id}completed
    Completes the assessment and sets the refferal status to OPEN

--- Fmas12d.Business/Services/Interfaces/IAssessmentService.cs
+++ Fmas12d.Business/Services/Interfaces/IAssessmentService.cs
@@ -10,6 +10,7 @@ namespace Fmas12d.Business.Services
     Task<IAssessmentDoctorsUpdate> AddAllocatedDoctorsAsync(IAssessmentDoctorsUpdate model);
     Task<IAssessmentDoctorsUpdate> AddAllocatedDoctorDirectAsync(int id, int userId);
     Task<IAssessmentDoctorsUpdate> AddSelectedDoctorsAsync(IAssessmentDoctorsUpdate model);
+    Task<bool> Complete(int id);
     Task<AssessmentCreate> CreateAsync(AssessmentCreate model);
     Task<IEnumerable<Assessment>> GetListByUserIdAsync(
       int userId,
