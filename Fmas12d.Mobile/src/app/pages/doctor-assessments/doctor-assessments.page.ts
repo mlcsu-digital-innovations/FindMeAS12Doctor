feefffc6865d4d3768ed1d68b7d4d3c26cee5363
Mark Carter <mark.carter4@nhs.net>
2020-08-27 13:24:52 +0100

    Hide old assessments Fixes AB#10823

--- Fmas12d.Mobile/src/app/pages/doctor-assessments/doctor-assessments.page.ts
+++ Fmas12d.Mobile/src/app/pages/doctor-assessments/doctor-assessments.page.ts
@@ -8,6 +8,8 @@ import { BroadcastService } from '@azure/msal-angular';
 import { Component } from '@angular/core';
 import { LoadingController, ToastController } from '@ionic/angular';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'app-doctor-assessments',
   templateUrl: './doctor-assessments.page.html',
@@ -24,6 +26,9 @@ export class DoctorAssessmentsPage {
 
   private loading: HTMLIonLoadingElement;
   private hasData: boolean;
+  public showAll: boolean;
+
+  private readonly scheduled = [ASSESSMENTSCHEDULED, ASSESSMENTRESCHEDULING, AWAITINGREVIEW];
 
   constructor(
     private assessmentService: AmhpAssessmentService,
@@ -34,6 +39,36 @@ export class DoctorAssessmentsPage {
 
   ionViewDidEnter() {
     this.refreshPage();
+  }
+
+  toggleChanged(showAll: boolean) {
+    this.filterData();
+  }
+
+  filterData() {
+
+    this.scheduledAssessments = [];
+    this.assessmentRequests = [];
+
+    // scheduled assessments will have a referralId of 6, 7 or 8
+    this.scheduledAssessments = this.allAssessments.filter
+      (assessment => this.scheduled.includes(assessment.referralStatusId));
+
+    this.assessmentRequests = this.allAssessments
+      .filter(assessment => !this.scheduled.includes(assessment.referralStatusId))
+      .filter(assessment => assessment.referralStatusId !== REFERRALSTATUSOPEN);
+
+    if (!this.showAll) {
+      const currentDate = moment().startOf('day');
+
+      this.scheduledAssessments =
+        this.scheduledAssessments
+        .filter(assessment => moment(assessment.dateTime).isAfter(currentDate));
+
+      this.assessmentRequests =
+        this.assessmentRequests
+        .filter(assessment => moment(assessment.dateTime).isAfter(currentDate));
+    }
   }
 
   refreshPage($event?: any) {
@@ -50,20 +85,7 @@ export class DoctorAssessmentsPage {
           this.allAssessments = result;
 
           if (result && result.length > 0) {
-
-            const scheduled = [ASSESSMENTSCHEDULED, ASSESSMENTRESCHEDULING, AWAITINGREVIEW];
-
-            // scheduled assessments will have a referralId of 6, 7 or 8
-            this.scheduledAssessments = this.allAssessments.filter
-              (assessment => scheduled.includes(assessment.referralStatusId));
-
-            this.assessmentRequests = this.allAssessments
-              .filter(assessment => !scheduled.includes(assessment.referralStatusId))
-              .filter(assessment => assessment.referralStatusId !== REFERRALSTATUSOPEN);
-
-          } else {
-            this.scheduledAssessments = [];
-            this.assessmentRequests = [];
+            this.filterData();
           }
 
           this.broadcastService.broadcast('assessments:requiringaction',
